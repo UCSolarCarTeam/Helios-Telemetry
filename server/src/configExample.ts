aaebc862efea9609020ab704690c349e35b434f3
JensVarughese <jensvarughese@gmail.com>
2023-01-21 15:03:07 -0700

    Updated example config

--- server/src/configExample.ts
+++ server/src/configExample.ts
@@ -1,11 +1,12 @@
-module.exports.rabbitmq = {
-  host: 'amqp://localhost:5672',
-  queue_name: 'msgQueue',
-  exchange_name: 'hermesExchange',
-};
-
-export class Config {
-  static mongoUri: string = '';
+export default class Config {
+  static mongoUri: string = 
+  'mongodb+srv://solarcaradmin:VKNsFdQRVcuqxfXq@uofcsolarcarcloud.yeteq.mongodb.net/UofCSolarCarCloud?retryWrites=true&w=majority';
   static port: number = 4000;
   static isProd: boolean = false;
+}
+
+export class RabbitMQ {
+  host: string =  'amqp://localhost:5672';
+  queue_name: string = 'msgQueue';
+  exchange_name: string = 'hermesExchange';
 }
