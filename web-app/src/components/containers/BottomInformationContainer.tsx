96fde49ed844c4d86b490bcd32fc06f31e49b312
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-02-04 15:53:26 -0700

    fix spacing add bar

--- web-app/src/components/containers/BottomInformationContainer.tsx
+++ web-app/src/components/containers/BottomInformationContainer.tsx
@@ -1,56 +1,65 @@
-import React from 'react'
+import React from "react";
 
-const fakeData = { 'Motor Temp': 55, 'Battery Power': 54.35, 'Motor': 55, 'Motor Temp1': 55, 'Motor Temp2': 55, 'Motor Temp3': 55, 'Pack Voltage': 55, MPPT: 55, 'Battery Current': 55, 'Bus Voltage': 55 }
+const fakeData = {
+  "Motor Temp": 55,
+  "Battery Power": 54.35,
+  Motor: 55,
+  "Motor Temp1": 55,
+  "Motor Temp2": 55,
+  "Motor Temp3": 55,
+  "Pack Voltage": 55,
+  MPPT: 55,
+  "Battery Current": 55,
+  "Bus Voltage": 55,
+};
 
-function BottomInformationContainer (props: any) {
+function BottomInformationContainer(props: any) {
   return (
     <div className="">
-      <div className='text-xl align-center flex flex-nowrap justify-between'>
-        <div className='col-span-1'>
-          <div className='text-xs'> Motor Temperature </div>
-          <div>{fakeData['Motor Temp']}</div>
+      <div className="h-px bg-black"></div>
+      <div className="text-xl flex flex-nowrap justify-evenly text-center py-5">
+        <div className="">
+          <div className="text-xs"> Motor Temperature </div>
+          <div>{fakeData["Motor Temp"]}</div>
         </div>
         <div>
           <div className="text-xs"> Battery Power </div>
-          <div>{fakeData['Battery Power']}</div>
+          <div>{fakeData["Battery Power"]}</div>
         </div>
         <div>
           <div className="text-xs"> Motor </div>
-          <div>{fakeData['Motor']}</div>
+          <div>{fakeData["Motor"]}</div>
         </div>
         <div>
           <div className="text-xs"> Motor Temperature </div>
-          <div>{fakeData['Motor Temp1']}</div>
+          <div>{fakeData["Motor Temp1"]}</div>
         </div>
         <div>
           <div className="text-xs"> Motor Temperature </div>
-          <div>{fakeData['Motor Temp2']}</div>
+          <div>{fakeData["Motor Temp2"]}</div>
         </div>
         <div>
           <div className="text-xs"> Motor Temperature </div>
-          <div>{fakeData['Motor Temp3']}</div>
+          <div>{fakeData["Motor Temp3"]}</div>
         </div>
         <div>
           <div className="text-xs"> Pack Voltage </div>
-          <div>{fakeData['Pack Voltage']}</div>
+          <div>{fakeData["Pack Voltage"]}</div>
         </div>
         <div>
           <div className="text-xs"> MPPT </div>
-          <div>{fakeData['MPPT']}</div>
+          <div>{fakeData["MPPT"]}</div>
         </div>
         <div>
           <div className="text-xs"> Battery Current </div>
-          <div>{fakeData['Battery Current']}</div>
+          <div>{fakeData["Battery Current"]}</div>
         </div>
         <div>
           <div className="text-xs"> Bus Voltage </div>
-          <div>{fakeData['Bus Voltage']}</div>
+          <div>{fakeData["Bus Voltage"]}</div>
         </div>
       </div>
     </div>
-
-
-
-  )
+  );
 }
-export default BottomInformationContainer
+export default BottomInformationContainer;
