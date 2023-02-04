64d828bbd4c98d1bfa91b9d82a68b0b5278af8f3
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-02-04 14:58:35 -0700

    fix spacing

--- web-app/src/components/containers/BottomInformationContainer.tsx
+++ web-app/src/components/containers/BottomInformationContainer.tsx
@@ -4,52 +4,51 @@ const fakeData = { 'Motor Temp': 55, 'Battery Power': 54.35, 'Motor': 55, 'Motor
 
 function BottomInformationContainer (props: any) {
   return (
-    <div className="grid w-full h-full bg-pink">
-
-      <div className='text-xl align-center flex flex-nowrap'>
+    <div className="">
+      <div className='text-xl align-center flex flex-nowrap justify-between'>
         <div className='col-span-1'>
-          <div> Motor Temperature </div>
+          <div className='text-xs'> Motor Temperature </div>
           <div>{fakeData['Motor Temp']}</div>
-        </div> 
+        </div>
         <div>
-          <div> Battery Power </div>
+          <div className="text-xs"> Battery Power </div>
           <div>{fakeData['Battery Power']}</div>
         </div>
         <div>
-          <div> Motor </div>
+          <div className="text-xs"> Motor </div>
           <div>{fakeData['Motor']}</div>
         </div>
         <div>
-          <div> Motor Temperature </div>
+          <div className="text-xs"> Motor Temperature </div>
           <div>{fakeData['Motor Temp1']}</div>
         </div>
         <div>
-          <div> Motor Temperature </div>
+          <div className="text-xs"> Motor Temperature </div>
           <div>{fakeData['Motor Temp2']}</div>
         </div>
         <div>
-          <div> Motor Temperature </div>
+          <div className="text-xs"> Motor Temperature </div>
           <div>{fakeData['Motor Temp3']}</div>
         </div>
         <div>
-          <div> Pack Voltage </div>
+          <div className="text-xs"> Pack Voltage </div>
           <div>{fakeData['Pack Voltage']}</div>
         </div>
         <div>
-          <div> MPPT </div>
+          <div className="text-xs"> MPPT </div>
           <div>{fakeData['MPPT']}</div>
         </div>
         <div>
-          <div> Battery Current </div>
+          <div className="text-xs"> Battery Current </div>
           <div>{fakeData['Battery Current']}</div>
         </div>
         <div>
-          <div> Bus Voltage </div>
+          <div className="text-xs"> Bus Voltage </div>
           <div>{fakeData['Bus Voltage']}</div>
         </div>
       </div>
     </div>
-      
+
 
 
   )
