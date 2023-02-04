b8cea8317337a4af41b0af5fd89ce61f681dba4b
Dillon Sahadevan <dillonapple7h@gmail.com>
2023-02-04 14:48:02 -0700

    Switch to husky

--- web-app/src/components/containers/BottomInformationContainer.tsx
+++ web-app/src/components/containers/BottomInformationContainer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function BottomInformationContainer (props: any) {
-  return (
-      <h1 className="bg-pink-300">Bottom Information</h1>
-  )
+function BottomInformationContainer(props: any) {
+  return <h1 className="bg-pink-300">Bottom Information</h1>;
 }
 
-export default BottomInformationContainer
+export default BottomInformationContainer;
--- web-app/src/components/containers/TabsContainer.tsx
+++ web-app/src/components/containers/TabsContainer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function TabsContainer (props: any) {
-  return (
-      <h1 className="bg-pink-300">Tabs Container</h1>
-  )
+function TabsContainer(props: any) {
+  return <h1 className="bg-pink-300">Tabs Container</h1>;
 }
 
-export default TabsContainer
+export default TabsContainer;
