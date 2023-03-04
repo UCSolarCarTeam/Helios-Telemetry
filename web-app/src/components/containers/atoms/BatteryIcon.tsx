6a3181161a9db989cd9761d93134d6341f81877a
Caleb <calebbourbonnais1@gmail.com>
2023-03-04 14:54:17 -0700

    Moved text to center of battery icon

--- web-app/src/components/containers/atoms/BatteryIcon.tsx
+++ web-app/src/components/containers/atoms/BatteryIcon.tsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from "react";
 function BatteryIconComponent(props: any) {
-    const [batteryLevel, setBatteryLevel] = useState<number>(50);
+  const [batteryLevel, setBatteryLevel] = useState<number>(38.5);
 
-    const [batteryStyleString, setBatteryStyleString] = useState<string>('');
-    useEffect(() => {
-
-        const nextString = `linear-gradient(
+  const [batteryStyleString, setBatteryStyleString] = useState<string>("");
+  useEffect(() => {
+    const nextString = `linear-gradient(
             90deg, 
             #9C0534 0%, #9C0534 ${batteryLevel}%, 
-            #BAB8B8 ${batteryLevel}%, #BAB8B8 50%`
-        setBatteryStyleString(nextString);
-    }, [batteryLevel]);
+            #BAB8B8 ${batteryLevel}%, #BAB8B8 50%`;
+    setBatteryStyleString(nextString);
+  }, [batteryLevel]);
 
-  return ( 
-    <div className="rounded" style={{backgroundImage: batteryStyleString}}>
-        <div className="text-6xl text-center text-[#FFFFFF] ">{batteryLevel.toString()}%</div>
-     </div>
+  return (
+    <div className="rounded" style={{ backgroundImage: batteryStyleString }}>
+      <div className="flex justify-center items-stretch text-1xl min-h-full text-[#FFFFFF]">
+        <div className="min-h-full self-center">{batteryLevel.toString()}%</div>
+      </div>
+    </div>
   );
 }
 
