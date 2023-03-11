4f9196ba42cc288df3393b861edd9aca03b97e8e
Caleb <calebbourbonnais1@gmail.com>
2023-03-11 11:27:06 -0700

    removed return for battery icon

--- web-app/src/components/molecules/HeroMolecules/BatteryThrottleComponent.tsx
+++ web-app/src/components/molecules/HeroMolecules/BatteryThrottleComponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BatteryIconComponent from "../../containers/atoms/BatteryIcon";
 
 function BatteryThrottleComponent(props: any) {
-  return <BatteryIconComponent/>
+  return <h1></h1>;
 }
 
 export default BatteryThrottleComponent;
