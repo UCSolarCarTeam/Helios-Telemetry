7c592e3f6848c950cb21a37d3ffe76ccd3df1bf5
jjason-nn <jason.nguyen2@ucalgary.ca>
2023-03-15 02:04:27 +0000

    Finished TabsContainer and NavBar

--- web-app/src/components/containers/TabsContainer.tsx
+++ web-app/src/components/containers/TabsContainer.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { routes, SolarCarRoutes } from "../../objects/TabRoutes";
+import NavBar from "../tabs/NavBar";
 
 function TabsContainer(props: any) {
   return (
-    <h1 className="text-gray border border-dashed border-black">
-      Tabs Container
+    <h1 className="">
+      <NavBar />
+      <Routes>
+        {routes.map((route: SolarCarRoutes, i: number) => (
+          <Route path={route.path} element={route.element} />
+        ))}
+      </Routes>
     </h1>
   );
 }
---
+++ web-app/src/components/tabs/NavBar.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import {
+  Routes,
+  RouteObject,
+  Link,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+
+import { routes, SolarCarRoutes } from "../../objects/TabRoutes";
+import path from "path";
+
+function NavBar(props: any) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const getCurrentPath = () => {
+    routes.map((route: SolarCarRoutes, i: number) => {
+      if (route.path === location.pathname) {
+        return route.value;
+      }
+    });
+    return "1";
+  };
+
+  const redirect = () => {
+    routes.map((route: SolarCarRoutes) => {
+      if (route.value === value) {
+        navigate(route.path.toString());
+      }
+    });
+  };
+
+  const [value, setValue] = React.useState<String>(getCurrentPath());
+
+  useEffect(() => {
+    redirect();
+  }, [value]);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: String) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Tabs value={value} variant="fullWidth" onChange={handleChange}>
+        {routes.map((route: SolarCarRoutes, i: number) => (
+          <Tab
+            key={i}
+            value={route.value}
+            label={<span style={{ color: "3A3A3A" }}>{route.id}</span>}
+          ></Tab>
+        ))}
+      </Tabs>
+    </>
+  );
+}
+
+export default NavBar;
