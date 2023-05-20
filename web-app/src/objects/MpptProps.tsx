f7bbde2f384a10c88f8280aa5806b8dbf6142f1b
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2023-05-20 13:59:12 -0600

    TEL-41-Design-MPPT-tab-in-Figma (#35)
    * Start on MPPT
    
    * show the numbers
    
    * add mppt display
    
    * add fake data
    
    * updated fake data
    
    * add underline
    
    * [dev] add missing FakerJS import 😔
    
    * added typing to mppt
    
    * update typing and style
    
    ---------
    
    Co-authored-by: brian-ngyn <brian.nguy1en@gmail.com>

---
+++ web-app/src/objects/MpptProps.tsx
@@ -0,0 +1,14 @@
+export interface ChannelData {
+  arrayVoltage: string;
+  arrayCurrent: string;
+  batteryVoltage: string;
+  temperature: string;
+}
+export interface UnitData {
+  Channel0: ChannelData;
+  Channel1: ChannelData;
+}
+export interface MpptData {
+  Unit0: UnitData;
+  Unit1: UnitData;
+}
