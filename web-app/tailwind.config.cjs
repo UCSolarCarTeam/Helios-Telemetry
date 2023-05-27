ef13fe531579385d18821e8bd93aab8e07e70d8c
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2023-05-27 13:26:45 -0600

    complete battery component (#36)
    * complete battery component
    
    * Add fake data as variables
    
    * create battery props object

--- web-app/tailwind.config.cjs
+++ web-app/tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'xxs': '0.6rem'
+      },
       colors: {
         'primary': '#9C0534',
         'green': '#01650B',
@@ -15,7 +18,9 @@ module.exports = {
       textColor: {
         'light': '#3A3A3A',
         'dark': '#D2D2D2',
-        'helios': '#B94A6C'
+        'helios': '#B94A6C',
+        'pink': '#9C0534',
+        'gray': '#807D7D'
       },
       backgroundColor: {
         'light': '#D2D2D2',
