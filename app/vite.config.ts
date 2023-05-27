507d2c4f0cd3de977f307d92b5c351a130b8a688
ideen1 <ibanijamali@gmail.com>
2023-05-27 14:16:59 -0600

    update run scriptsd and build output

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,5 +6,8 @@ import svgrPlugin from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './src/renderer',
+  build: {
+    outDir: '../../dist'
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()]
 })
