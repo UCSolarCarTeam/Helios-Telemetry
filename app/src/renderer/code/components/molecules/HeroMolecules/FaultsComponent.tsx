f11de3a70957efc14fb9b4eb29c33dd43e57119c
ideen1 <ibanijamali@gmail.com>
2023-06-01 20:01:17 -0600

    Add motion-framer

--- app/src/renderer/code/components/molecules/HeroMolecules/FaultsComponent.tsx
+++ app/src/renderer/code/components/molecules/HeroMolecules/FaultsComponent.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import FaultCard from "../../atoms/FaultCard";
 import { ISeverity } from "../../atoms/FaultCard";
+import { motion } from "framer-motion";
 
 function FaultsComponent(props: any) {
+  {}
   return (
     <div>
-      <div className="h-[15%]">
+      <motion.div className="h-[15%]" animate={{ y: 0, opacity:1 }} initial={{ y: 100, opacity:0 }}>
         <FaultCard severity={ISeverity.WARNING} faultName={"FAULT 1"} />
-      </div>
-      <div className="h-[15%]">
+      </motion.div>
+      <motion.div className="h-[15%]" animate={{ y: 0 }} initial={{ y: 100 }}>
         <FaultCard severity={ISeverity.ERROR} faultName={"FAULT 2"} />
-      </div>
-    </div>
+      </motion.div>
+    </div >
   );
 }
 
