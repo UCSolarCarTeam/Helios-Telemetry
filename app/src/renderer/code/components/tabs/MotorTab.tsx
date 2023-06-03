0be16ae97742c0464a6ff8549d6398e97b3e8607
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-06-03 14:19:29 -0600

    update fakeData with faker

--- app/src/renderer/code/components/tabs/MotorTab.tsx
+++ app/src/renderer/code/components/tabs/MotorTab.tsx
@@ -1,57 +1,57 @@
-import React from 'react'
+import { faker } from '@faker-js/faker'
 import { IKeyMotor, IMotorDetail } from '../../objects/telemetry-data.interface'
 
 const fakeKeyMotor0Data: IKeyMotor = {
-  Alive: false,
-  SetCurrent: 0,
-  SetVelocity: 0,
-  BusCurrent: 0,
-  BusVoltage: 0,
-  VehicleVelocity: 0
+  Alive: faker.datatype.boolean(),
+  SetCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SetVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusVoltage: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VehicleVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 })
 }
 const fakeMotor0DetailsData: IMotorDetail = {
-  PhaseCCurrent: 0,
-  PhaseBCurrent: 0,
-  MotorVoltageReal: 0,
-  MotorVoltageImaginary: 0,
-  MotorCurrentReal: 0,
-  MotorCurrentImaginary: 0,
-  BackEmf: 0,
-  VoltageRail15VSupply: 0,
-  VoltageRail3VSupply: 0,
-  VoltageRail1VSupply: 0,
-  HeatSinkTemp: 0,
-  MotorTemp: 0,
-  DspBoardTemp: 0,
-  DcBusAmpHours: 0,
-  Odometer: 0,
-  SlipSpeed: 0
+  PhaseCCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  PhaseBCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BackEmf: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail15VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail3VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail1VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  HeatSinkTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DspBoardTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DcBusAmpHours: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  Odometer: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SlipSpeed: faker.number.float({ min: 0, max: 100, precision: 0.01 })
 }
 const fakeKeyMotor1Data: IKeyMotor = {
-  Alive: false,
-  SetCurrent: 0,
-  SetVelocity: 0,
-  BusCurrent: 0,
-  BusVoltage: 0,
-  VehicleVelocity: 0
+  Alive: faker.datatype.boolean(),
+  SetCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SetVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusVoltage: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VehicleVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 })
 }
 const fakeMotor1DetailsData: IMotorDetail = {
-  PhaseCCurrent: 0,
-  PhaseBCurrent: 0,
-  MotorVoltageReal: 0,
-  MotorVoltageImaginary: 0,
-  MotorCurrentReal: 0,
-  MotorCurrentImaginary: 0,
-  BackEmf: 0,
-  VoltageRail15VSupply: 0,
-  VoltageRail3VSupply: 0,
-  VoltageRail1VSupply: 0,
-  HeatSinkTemp: 0,
-  MotorTemp: 0,
-  DspBoardTemp: 0,
-  DcBusAmpHours: 0,
-  Odometer: 0,
-  SlipSpeed: 0
+  PhaseCCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  PhaseBCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BackEmf: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail15VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail3VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail1VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  HeatSinkTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DspBoardTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DcBusAmpHours: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  Odometer: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SlipSpeed: faker.number.float({ min: 0, max: 100, precision: 0.01 })
 }
 const fakeData = {
   Motor0: {
@@ -68,7 +68,9 @@ function MotorTab() {
     <>
       <p>Motor Tab </p>
       <div className="flex flex-nowrap justify-evenly text-center h-full">
-        Motor0 SetCurrent:{fakeData.Motor0.KeyMotor.SetCurrent}
+        Motor0 SetCurrent: {fakeData.Motor0.KeyMotor.SetCurrent}
+        <br />
+        Motor1 PhaseCCurrent: {fakeData.Motor1.MotorDetail.PhaseCCurrent}
       </div>
     </>
   )
