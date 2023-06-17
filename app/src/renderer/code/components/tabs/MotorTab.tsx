743e77de7387ef3471a7cf53ef49203965009d7a
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2023-06-17 13:50:29 -0600

    TEL-18-create-motor-tab-component (#38)
    * no change at all
    
    * add fake motor data
    
    * update fakeData with faker
    
    * Create Motor details components
    
    * setup layout for motor containers
    
    * Add some notes to do for next week
    
    * motorDetails container
    
    * motorKeyDetails container
    
    * motorTab component
    
    * motorAlive color indicator

--- app/src/renderer/code/components/tabs/MotorTab.tsx
+++ app/src/renderer/code/components/tabs/MotorTab.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import { faker } from '@faker-js/faker'
+import { IKeyMotor, IMotorDetail } from '../../objects/telemetry-data.interface'
+import MotorKeyDetailsContainer from '../containers/MotorKeyDetailsContainer'
+import MotorDetailsContainer from '../containers/MotorDetailsContainer'
 
-function MotorTab() {
-  return <p>Motor Tab</p>;
+const fakeKeyMotor0Data: IKeyMotor = {
+  Alive: faker.datatype.boolean(),
+  SetCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SetVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusVoltage: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VehicleVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 })
+}
+const fakeMotor0DetailsData: IMotorDetail = {
+  PhaseCCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  PhaseBCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BackEmf: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail15VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail3VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail1VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  HeatSinkTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DspBoardTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DcBusAmpHours: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  Odometer: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SlipSpeed: faker.number.float({ min: 0, max: 100, precision: 0.01 })
+}
+const fakeKeyMotor1Data: IKeyMotor = {
+  Alive: faker.datatype.boolean(),
+  SetCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SetVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BusVoltage: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VehicleVelocity: faker.number.float({ min: 0, max: 100, precision: 0.01 })
+}
+const fakeMotor1DetailsData: IMotorDetail = {
+  PhaseCCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  PhaseBCurrent: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorVoltageImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentReal: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorCurrentImaginary: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  BackEmf: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail15VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail3VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  VoltageRail1VSupply: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  HeatSinkTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  MotorTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DspBoardTemp: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  DcBusAmpHours: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  Odometer: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  SlipSpeed: faker.number.float({ min: 0, max: 100, precision: 0.01 })
+}
+const fakeData = {
+  Motor0: {
+    KeyMotor: fakeKeyMotor0Data,
+    MotorDetail: fakeMotor0DetailsData
+  },
+  Motor1: {
+    KeyMotor: fakeKeyMotor1Data,
+    MotorDetail: fakeMotor1DetailsData
+  }
+}
+function MotorTab(): JSX.Element {
+  return (
+    <>
+      {/* I see 2 containers  */}
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-col w-1/2">
+          {/* add 2 columns in each details container instead of justify-between. 
+          define the width to be half the parent container.
+          Instead of gap-8, use justify-between*/}
+          <MotorKeyDetailsContainer KeyMotorDetails={fakeData.Motor0.KeyMotor} direction={'Left'} />
+          <MotorDetailsContainer MotorDetails={fakeData.Motor0.MotorDetail} direction={'Left'} />
+        </div>
+        <div className="flex flex-col w-1/2">
+          <MotorKeyDetailsContainer KeyMotorDetails={fakeData.Motor1.KeyMotor} direction="Right" />
+          <MotorDetailsContainer MotorDetails={fakeData.Motor1.MotorDetail} direction={'Right'} />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default MotorTab;
+export default MotorTab
