e907863ff978656b1e452bd8a90e6bf3bb8d643e
ideen1 <ibanijamali@gmail.com>
2023-07-22 13:03:45 -0600

    Map follow edits

--- app/src/renderer/code/components/molecules/MapMolecules/MapText.tsx
+++ app/src/renderer/code/components/molecules/MapMolecules/MapText.tsx
@@ -1,22 +1,35 @@
-import React from "react";
-
-function MapText() {
-  const lapsLeft = 0;
-  const timeLeft = 0;
+type MapTextProps = {
+  setFollowMap: (value: boolean) => void
+  followMap: boolean
+}
+function MapText(props: MapTextProps): JSX.Element {
+  const { setFollowMap, followMap } = props
+  const lapsLeft = 0
+  const timeLeft = 0
   return (
     <>
       <div className="w-full text-center mt-1">
-        <div className="grid grid-cols-2">
-          <div className="grid col-span-1">
+        <div className="flex justify-between w-full">
+          <div className="flex">
+            <p>Follow </p>
+            <input
+              type="checkbox"
+              name="follow"
+              id="follow"
+              checked={followMap}
+              onClick={(): void => setFollowMap(!followMap)}
+            />
+          </div>
+          <div className="flex">
             <p>Laps Left: {lapsLeft}</p>
           </div>
-          <div className="grid col-span-1">
+          <div className="flex">
             <p>Time Left: {timeLeft}</p>
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default MapText;
+export default MapText
