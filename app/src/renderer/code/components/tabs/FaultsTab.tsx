3c0883b308c1eca5a40abdf4ddfe4a905a607748
ideen1 <ibanijamali@gmail.com>
2024-01-29 18:35:27 -0700

    Pis boolean fixes

--- app/src/renderer/code/components/tabs/FaultsTab.tsx
+++ app/src/renderer/code/components/tabs/FaultsTab.tsx
@@ -1,12 +1,12 @@
 import PIS from "../../objects/PIS/PIS";
 import Battery from "../../objects/PIS/PIS.battery";
-import PIStransformers from "../transformers/PIStransformer";
+import PISTransformers from "../transformers/PISTransformer";
 
 function FaultsTab(): JSX.Element {
   return (
     <div>
       <p>Faults Tab</p>
-      <PIStransformers root={Battery()} />
+      <PISTransformers root={Battery()} />
     </div>
   );
 }
--- app/src/renderer/code/components/transformers/PIStransformer.tsx
+++ app/src/renderer/code/components/transformers/PIStransformer.tsx
@@ -6,28 +6,56 @@ import type {
 type RangeCheckedFieldDataProps = {
   fieldData: I_PISFieldData;
 };
+
 function RangeCheckedFieldData(props: RangeCheckedFieldDataProps): JSX.Element {
   const { value, unit, min, max, expectedBool } = props.fieldData;
   const inRange =
     // If value is of type string range is true
-    typeof value == "string"
+    typeof value === "string"
       ? true
       : // If value is of type number range is true if min and max are undefined or value is between min and max
-        typeof value == "number"
+        typeof value === "number"
         ? (min === undefined || value >= min) &&
           (max === undefined || value <= max)
         : // If value is of type boolean range is true if expectedBool is undefined and value is false or value is equal to expectedBool
-          typeof value == "boolean"
+          typeof value === "boolean"
           ? (expectedBool === undefined && value === false) ||
             expectedBool === value
           : false;
 
-  const color = inRange ? "text-green-500" : "text-red-500";
+  const color = inRange ? "text-green" : "text-red-500";
+  const displayValue = typeof value === "boolean" ? (value ? "T" : "F") : value;
 
   return (
     <span className={color}>
-      {value}
+      {displayValue}
       {unit}
+    </span>
+  );
+}
+
+type FormatStringProps = {
+  fstring: string;
+  data: I_PISFieldData[];
+};
+
+function FormatString(props: FormatStringProps): JSX.Element {
+  const { fstring, data } = props;
+  // %s •C (%s) - %s •C (%s)
+  // console.log("TEST", fstring.split("%s"));
+
+  return (
+    <span>
+      {fstring.split("%s").map((part, index) => {
+        return index === fstring.split("%s").length - 1 ? (
+          <span key={index}>{part}</span>
+        ) : (
+          <span key={index}>
+            {part}
+            <RangeCheckedFieldData fieldData={data[index]} />
+          </span>
+        );
+      })}
     </span>
   );
 }
@@ -61,7 +89,9 @@ function FieldDataFormatter(props: FieldDataFormatterProps): JSX.Element {
       <RangeCheckedFieldData fieldData={data[0]} />
     </div>
   ) : (
-    <div>{formatString(fstring, data)}</div>
+    <div>
+      <FormatString fstring={fstring} data={data} />
+    </div>
   );
 }
 
@@ -81,7 +111,7 @@ function FieldPrinter(props: FieldPrinterProps): JSX.Element {
     return <div>PIS ERROR: </div>;
   }
   return (
-    <div>
+    <div className="mt-1 flex items-center justify-between text-xs">
       {field.name}:
       <FieldDataFormatter data={field.data} fstring={field.fstring} />
     </div>
@@ -107,34 +137,36 @@ type PIStransformerProps = {
   depth?: number;
 };
 
-function PIStransformer(props: PIStransformerProps): JSX.Element {
+function PISTransformer(props: PIStransformerProps): JSX.Element {
   const { root, depth = 0 } = props;
   console.log(root);
   return (
-    <div className="flex w-full justify-between gap-4 ">
-      {Object.keys(root).map((key, index) => {
-        const value = root[key];
-        return (
-          <div key={index} className={`flex flex-col `}>
-            <div className="flex w-full items-center justify-evenly border-b-2 border-helios">
-              <p
-                className={`text-helios ${
-                  depth >= 2 ? `text-xs` : depth === 1 ? "text-sm" : "text-lg"
-                }`}
-              >
-                {key}
-              </p>
+    root && (
+      <div className="flex w-full justify-between gap-4 ">
+        {Object.keys(root).map((key, index) => {
+          const value = root[key];
+          return (
+            <div key={index} className={`flex flex-col `}>
+              <div className="flex w-full items-center justify-evenly border-b-2 border-helios">
+                <p
+                  className={`pt-3 font-bold text-helios ${
+                    depth >= 2 ? `text-xs` : depth == 1 ? "text-sm" : "text-lg"
+                  }`}
+                >
+                  {key}
+                </p>
+              </div>
+              {Array.isArray(value) ? (
+                <FieldsPrinter fields={value} />
+              ) : (
+                <PISTransformer root={value} depth={depth + 1} />
+              )}
             </div>
-            {Array.isArray(value) ? (
-              <FieldsPrinter fields={value} />
-            ) : (
-              <PIStransformer root={value} depth={depth + 1} />
-            )}
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    )
   );
 }
 
-export default PIStransformer;
+export default PISTransformer;
