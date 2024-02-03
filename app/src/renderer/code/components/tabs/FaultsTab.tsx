a059a65714f3cfa0e7adee03330f9f6ae182d785
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-03 11:03:45 -0700

    Fix import statement in FaultsTab component

--- app/src/renderer/code/components/tabs/FaultsTab.tsx
+++ app/src/renderer/code/components/tabs/FaultsTab.tsx
@@ -1,11 +1,11 @@
 import PIS from "../../objects/PIS/PIS";
 import Battery from "../../objects/PIS/PIS.battery";
-import PISTransformers from "../transformers/PISTransformer";
+import PISTransformer from "../transformers/PISTransformer";
 
 function FaultsTab(): JSX.Element {
   return (
     <div>
-      <PISTransformers root={Battery()} />
+      <PISTransformer root={Battery()} />
     </div>
   );
 }
