94a8cf4552fa62d926bf5d2b80679d846b2e0511
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-03-16 22:35:10 +0000

    Co-authored-by: Ideen <ideen1@users.noreply.github.com> Co-authored-by: brian nguyen <brian.nguyen3@ucalgary.ca>

--- client/src/components/molecules/GraphMolecules/GraphContainer.tsx
+++ client/src/components/molecules/GraphMolecules/GraphContainer.tsx
@@ -1,56 +1,23 @@
-import React from "react";
+import React, { type MutableRefObject } from "react";
 
 import DraggableGraph from "@/components/molecules/GraphMolecules/DraggableGraph";
+import { type AnchorElTooltipsRefHandle } from "@/components/transformers/PISTransformer";
 import { useGraphOverlay } from "@/contexts/GraphOverlayContext";
 
 export default function GraphContainer() {
   const { openGraphs, closeGraph } = useGraphOverlay();
-  const data = [
-    {
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+
   return (
     <div className="absolute z-50 m-auto">
       {openGraphs.map((graph) => {
         return (
           <DraggableGraph
-            data={data}
+            myRef={
+              graph.ref as MutableRefObject<AnchorElTooltipsRefHandle | null>
+            }
             closeGraph={closeGraph}
-            graphID={graph}
-            key={graph}
+            graphID={graph.name}
+            key={graph.name}
           />
         );
       })}
