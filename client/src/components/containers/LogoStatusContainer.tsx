5c668152a8af81cbcf0809f9d6e322eca39adc93
JenniferJa <102393847+JenniferJa@users.noreply.github.com>
2024-03-16 22:02:09 +0000

    modal with toggles made, dark to light functionality implemented

--- client/src/components/containers/LogoStatusContainer.tsx
+++ client/src/components/containers/LogoStatusContainer.tsx
@@ -1,11 +1,13 @@
 import LogoComponent from "@/components/molecules/LogoStatusMolecules/LogoComponent";
 import StatusComponent from "@/components/molecules/LogoStatusMolecules/StatusComponent";
+import SettingsComponent from "../molecules/LogoStatusMolecules/SettingsComponent";
 
 function LogoStatusContainer() {
   return (
     <>
       <LogoComponent />
       <StatusComponent />
+      <SettingsComponent/>
     </>
   );
 }
