6d6bb114bd7a3d3558d463e01c655fb65795d1b2
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-06 21:53:36 -0600

    add socketIO client events

--- client/src/socket/index.ts
+++ client/src/socket/index.ts
@@ -2,12 +2,15 @@ import { type Socket, io } from "socket.io-client";
 
 interface ClientToServerEvents {
   test: () => void;
+  "create-something": (value: string, callback: () => void) => void;
 }
 
 interface ServerToClientEvents {
   noArg: () => void;
   basicEmit: (a: number, b: string, c: Buffer) => void;
   withAck: (d: string, callback: (e: number) => void) => void;
+  foo: (value: string) => void;
+  time: (value: string) => void;
 }
 
 export const socketIO: Socket<ServerToClientEvents, ClientToServerEvents> = io(
