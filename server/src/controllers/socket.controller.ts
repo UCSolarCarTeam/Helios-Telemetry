7fdf7c3245440d2d9e0d4dd6a5d88cc34174e9f0
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-06 21:52:16 -0600

    added socketIO test server events

--- server/src/controllers/socket.controller.ts
+++ server/src/controllers/socket.controller.ts
@@ -1,4 +1,4 @@
-import { type Socket } from "socket.io";
+import { Server, type Socket } from "socket.io";
 
 import { createLightweightApplicationLogger } from "@/utils/logger";
 
@@ -7,11 +7,27 @@ const logger = createLightweightApplicationLogger("socket.controller.ts");
 export const handleSocketConnection = (socket: Socket) => {
   logger.info("Client connected");
 
+  const timerId = setInterval(() => {
+    const now = new Date(); // Get the current time
+    socket.emit("time", now.toString()); // Emit the current time to the client
+    logger.info(`Time sent to client: ${now.toString()}`);
+  }, 5000); // Set the interval to 5000 milliseconds (5 seconds)
+
   socket.on("test", () => {
     logger.info("Test event received");
   });
 
   socket.on("disconnect", () => {
     logger.info("Client disconnected");
+    clearInterval(timerId);
+  });
+
+  socket.on("chat message", (msg) => {
+    socket.emit("chat message", msg);
+  });
+
+  socket.on("create-something", (value) => {
+    logger.info(`create-something event received with value: ${value}`);
+    socket.emit("foo", `Received value: ${value}`);
   });
 };
