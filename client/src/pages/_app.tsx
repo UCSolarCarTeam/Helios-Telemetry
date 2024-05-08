446806f2fb6deca11adf996ae19f09bfffe27f20
jenniferja <jenniferjaystar2@gmail.com>
2024-05-08 16:16:45 -0600

    car drives in circles using timer

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,56 @@
 import type { AppProps } from "next/app";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 import { AppStateContextProvider } from "@/contexts/AppStateContext";
 import { PacketContextProvider } from "@/contexts/PacketContext";
 import "@/styles/globals.css";
 
+function LoadingSpinner() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-200">
+      <div
+        className="animate-circle"
+        style={{ width: "0.2in", height: "0.2in", position: "relative" }}
+      >
+        <div
+          className="absolute left-1/2 top-1/2"
+          style={{
+            transform: "translate(-50%, -50%)",
+            width: "20px",
+            height: "20px",
+          }}
+        >
+          <Image
+            src="/assets/HeliosBirdseye.png"
+            alt="Loading..."
+            width={100}
+            height={100}
+            style={{
+              animation: "circle 2s linear infinite", // Apply circular from global.css
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 50000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <AppStateContextProvider>
       <PacketContextProvider>
-        <Component {...pageProps} />
+        {loading ? <LoadingSpinner /> : <Component {...pageProps} />}
       </PacketContextProvider>
     </AppStateContextProvider>
   );
