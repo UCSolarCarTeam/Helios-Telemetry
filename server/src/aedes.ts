b75806b6bfe2a2bd014424233390e9f91d0930eb
jenniferja <jenniferjaystar2@gmail.com>
2024-05-18 13:53:25 -0600

    aedes received message from client and logs it

--- server/src/aedes.ts
+++ server/src/aedes.ts
@@ -1,9 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
+import { createLightweightApplicationLogger } from "@/utils/logger";
+
+const logger = createLightweightApplicationLogger("aedes.ts");
 const Aedes = require("aedes");
 const { createServer } = require("net");
 
 const aedes = new Aedes();
+aedes.on("publish", async (packet, client) => {
+  if (client) {
+    logger.info(
+      `Published message from client ${client.id}: ${packet.payload.toString()}`,
+    );
+  }
+});
+
 const server = createServer(aedes.handle);
 
 export default server;
