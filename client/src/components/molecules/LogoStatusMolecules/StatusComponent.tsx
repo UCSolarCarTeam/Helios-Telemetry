c6ba4f154e12f31563002efbf38ef103cd6fd085
Burton Jong <burtonjong05@gmail.com>
2024-06-01 13:34:06 -0600

    First commit

--- client/src/components/molecules/LogoStatusMolecules/StatusComponent.tsx
+++ client/src/components/molecules/LogoStatusMolecules/StatusComponent.tsx
@@ -1,3 +1,4 @@
+
 function StatusComponent() {
   return (
     <div className="grid">
@@ -12,6 +13,10 @@ function StatusComponent() {
       <div className="grid w-fit grid-cols-2 gap-4">
         <h5 className="text-text-gray dark:text-text-gray-dark">STATUS</h5>
         <div className="bg-green dark:bg-green-dark mt-1 size-5 rounded-full"></div>
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        {" "}
+        <h1>hi</h1>
       </div>
     </div>
   );
