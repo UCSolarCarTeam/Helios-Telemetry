4213c5241319d70ff4d0ddd7a670514439d92725
David-Rodriguez-Barrios <david.rodriguezbarri@ucalgary.ca>
2024-06-01 13:07:17 -0600

    Fix: Implemented the refocus when the car's location and the flag location are not visible. Still need to figure out how to implement debounce to make it smoother.

--- client/src/components/molecules/MapMolecules/Map.tsx
+++ client/src/components/molecules/MapMolecules/Map.tsx
@@ -1,6 +1,8 @@
+import type { LngLatBoundsLike } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Image from "next/image";
-import ReactMap, { Marker } from "react-map-gl";
+import { useEffect, useRef } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 
 import SportsScoreIcon from "@mui/icons-material/SportsScore";
 
@@ -15,54 +17,71 @@ type IMapProps = {
   lapLocation: ILocation;
 };
 
+const fitToBounds = (
+  map: mapboxgl.Map,
+  carLocation: ILocation,
+  lapLocation: ILocation,
+) => {
+  const bounds: LngLatBoundsLike = [
+    [carLocation.lng, carLocation.lat],
+    [lapLocation.lng, lapLocation.lat],
+  ];
+
+  map.fitBounds(bounds, {
+    padding: { top: 35, bottom: 35, left: 35, right: 35 },
+    linear: true,
+  });
+};
 function Map(props: IMapProps): JSX.Element {
   const { carLocation, mapLocation, lapLocation } = props;
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  useEffect(() => {
+    if (mapRef.current) {
+      fitToBounds(mapRef.current, carLocation, lapLocation);
+    }
+  }, [carLocation, lapLocation]);
 
   if (!process.env.NEXT_PUBLIC_MAPSAPIKEY) return <></>;
   return (
-    <ReactMap
-      mapLib={import("mapbox-gl")}
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPSAPIKEY}
-      longitude={mapLocation.lng}
-      latitude={mapLocation.lat}
-      initialViewState={{
-        longitude: mapLocation.lng,
-        latitude: mapLocation.lat,
-        zoom: 14,
-      }}
-      style={{ width: "100%", height: "100%" }}
-      mapStyle="mapbox://styles/mapbox/dark-v11"
-      boxZoom={false}
-      doubleClickZoom={false}
-      dragPan={false}
-      dragRotate={false}
-      scrollZoom={false}
-      keyboard={false}
-      zoom={13}
-    >
-      <Marker
-        longitude={carLocation.lng}
-        latitude={carLocation.lat}
-        anchor={"center"}
+    <div style={{ width: "100%", height: "100%" }}>
+      <ReactMapGL
+        mapLib={import("mapbox-gl")}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPSAPIKEY}
+        initialViewState={{
+          longitude: mapLocation.lng,
+          latitude: mapLocation.lat,
+          zoom: 14,
+        }}
+        style={{ width: "100%", height: "100%" }}
+        mapStyle="mapbox://styles/mapbox/dark-v11"
+        boxZoom={false}
+        doubleClickZoom={false}
+        dragPan={false}
+        dragRotate={false}
+        scrollZoom={false}
+        keyboard={false}
+        onLoad={(event) => {
+          mapRef.current = event.target;
+          fitToBounds(mapRef.current, carLocation, lapLocation);
+        }}
       >
-        <Image
-          src="/assets/HeliosBirdseye.png"
-          alt="map-pin"
-          width={20}
-          height={50}
-        />
-      </Marker>
-      <Marker
-        longitude={lapLocation.lng}
-        latitude={lapLocation.lat}
-        anchor={"center"}
-        style={{ color: "white" }}
-        draggable={false}
-        pitchAlignment="auto"
-      >
-        <SportsScoreIcon />
-      </Marker>
-    </ReactMap>
+        <Marker longitude={carLocation.lng} latitude={carLocation.lat}>
+          <Image
+            src="/assets/HeliosBirdseye.png"
+            alt="map-pin"
+            width={20}
+            height={50}
+          />
+        </Marker>
+        <Marker
+          longitude={lapLocation.lng}
+          latitude={lapLocation.lat}
+          style={{ color: "white" }}
+        >
+          <SportsScoreIcon />
+        </Marker>
+      </ReactMapGL>
+    </div>
   );
 }
 
