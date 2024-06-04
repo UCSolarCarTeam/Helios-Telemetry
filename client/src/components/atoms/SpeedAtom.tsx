0e8d5ca3387ffa18fbc74ca5f3fc650c4c59310f
Burton Jong <108391733+burtonjong@users.noreply.github.com>
2024-06-03 18:38:53 -0600

    mobile view fixed (#94)
    * Amplify working code
    
    * Final Backend Deployment cahnges
    
    * Bump next from 14.1.0 to 14.1.1 (#88)
    
    Bumps [next](https://github.com/vercel/next.js) from 14.1.0 to 14.1.1.
    - [Release notes](https://github.com/vercel/next.js/releases)
    - [Changelog](https://github.com/vercel/next.js/blob/canary/release.js)
    - [Commits](https://github.com/vercel/next.js/compare/v14.1.0...v14.1.1)
    
    ---
    updated-dependencies:
    - dependency-name: next
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Tel 107 add unit conversion to pis transformer (#86)
    
    * started unit conversion logic
    
    * Create dockerfile for backend (#77)
    
    * make the CarGraphicComponent SSR instead of the HeroComponent (#80)
    
    * local storage and darkmode settings implemented
    
    * Create dockerfile for backend (#77)
    
    * make the CarGraphicComponent SSR instead of the HeroComponent (#80)
    
    * local storage and darkmode settings implemented
    
    * continued unit conversion function
    
    Co-authored-by: JenniferJa <JenniferJa@users.noreply.github.com>
    
    * finsihed unit handler function without changes from setting modal changes
    
    * yarn issue fixed by brian
    
    * local storage and darkmode settings implemented
    
    * yarn issue fixed by brian
    
    * moved enum to appStateContext
    
    * fixed small issue with displaying units
    
    * fixed PR comments
    
    * made unit conversion logic work with setting modal
    
    * made speed adapt value adapt to change in units
    
    * addressed comments. Changed to switch, case and destructured appState
    
    * changed to switch
    
    * fixed bug with undefined units and addressed comment
    
    * fixed the miles per hour to 0 decimal places
    
    ---------
    
    Co-authored-by: alexwhelan12 <alexwh2005.12@gmail.com>
    Co-authored-by: brian nguyen <brian.nguy1en@gmail.com>
    Co-authored-by: jenniferja <jenniferjaystar2@gmail.com>
    Co-authored-by: JenniferJa <JenniferJa@users.noreply.github.com>
    
    * mobile view fixed
    
    * some changes
    
    * Should be good
    
    * change to scrollbars
    
    * scrollbar spacing
    
    * oops
    
    * please im begging
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: ideen1 <ibanijamali@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Ideen <48538139+ideen1@users.noreply.github.com>
    Co-authored-by: alexwhelan12 <alexwh2005.12@gmail.com>
    Co-authored-by: brian nguyen <brian.nguy1en@gmail.com>
    Co-authored-by: jenniferja <jenniferjaystar2@gmail.com>
    Co-authored-by: JenniferJa <JenniferJa@users.noreply.github.com>

--- client/src/components/atoms/SpeedAtom.tsx
+++ client/src/components/atoms/SpeedAtom.tsx
@@ -7,12 +7,12 @@ function SpeedAtom(props: any) {
   return (
     <>
       <div className="col-span-2 grid h-10 w-full content-center justify-items-center">
-        <div className="grid grid-cols-2">
-          <div className="col-span-1 grid">
-            <h1 className="text-4xl">{speed}</h1>
+        <div className="flex flex-row">
+          <div className="items-center">
+            <h1 className="text-4xl">{speedValue.toFixed(0)}</h1>
           </div>
-          <div className="col-span-1 grid">
-            <h1 className="text-sm">km/h</h1>
+          <div className="items-center">
+            <h1 className="text-sm">{speedUnit}</h1>
           </div>
         </div>
       </div>
