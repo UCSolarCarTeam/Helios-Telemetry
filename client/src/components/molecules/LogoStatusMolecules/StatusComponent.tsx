80675e7c7e10a361bef3f86dba740235b6dc8797
Burton Jong <burtonjong05@gmail.com>
2024-06-03 20:39:26 -0600

    format change

--- client/src/components/molecules/LogoStatusMolecules/StatusComponent.tsx
+++ client/src/components/molecules/LogoStatusMolecules/StatusComponent.tsx
@@ -13,32 +13,32 @@ function StatusComponent() {
   return (
     <div className="grid">
       <div>
+        <div className="flex flex-row items-end justify-start pb-1 pt-2">
+          {" "}
+          <UserComputerIcon color={"#000000"} width="25px" height="25px" />
+          <LatencyDotsIcon
+            color={"#000000"}
+            width="15px"
+            height="20px"
+            latency={userLatency}
+            isConnected={userConnection}
+          />
+          <AWSIcon color={"#000000"} width="25px" height="25px" />
+          <LatencyDotsIcon
+            color={"#000000"}
+            width="15px"
+            height="20px"
+            latency={carLatency}
+            isConnected={carConnection}
+          />
+          <CarIcon color={"#000000"} width="25px" height="25px" />
+        </div>
         <h5 className="text-text-gray dark:text-text-gray-dark pb-1 text-sm ">
           PACKET TIMESTAMP
         </h5>
-        <h5 className="text-text-gray dark:text-text-gray-dark decoration-primary pb-3 text-xs underline decoration-1 underline-offset-4">
+        <h5 className="text-text-gray dark:text-text-gray-dark decoration-primary pb-2 text-xs underline decoration-1 underline-offset-4">
           Oct. 15 2022, 4:18.16pm
         </h5>
-      </div>
-      <div className="flex flex-row items-end justify-start">
-        {" "}
-        <UserComputerIcon color={"#000000"} width="25px" height="25px" />
-        <LatencyDotsIcon
-          color={"#000000"}
-          width="15px"
-          height="20px"
-          latency={userLatency}
-          isConnected={userConnection}
-        />
-        <AWSIcon color={"#000000"} width="25px" height="25px" />
-        <LatencyDotsIcon
-          color={"#000000"}
-          width="15px"
-          height="20px"
-          latency={carLatency}
-          isConnected={carConnection}
-        />
-        <CarIcon color={"#000000"} width="25px" height="25px" />
       </div>
     </div>
   );
