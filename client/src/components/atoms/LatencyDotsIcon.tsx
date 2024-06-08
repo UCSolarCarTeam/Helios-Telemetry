6e19dcc7c0d162473f30da82b9c2c2f51d5915a5
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-06-08 16:12:42 -0600

    feat: Add latency dots icon component with dynamic color and connection status

--- client/src/components/atoms/LatencyDotsIcon.tsx
+++ client/src/components/atoms/LatencyDotsIcon.tsx
@@ -1,3 +1,4 @@
+import { IoClose } from "react-icons/io5";
 import { twMerge } from "tailwind-merge";
 
 import { type SvgProps } from "@/components/atoms/SVGProps";
@@ -8,37 +9,46 @@ const LatencyDotsIcon = (props: SvgProps) => {
     <div className="flex flex-col items-center">
       <p
         className={twMerge(
-          "text-xs",
-          isConnected ? "text-green" : "text-red-500",
+          "w-6 text-xs",
+          isConnected ? "text-green" : "text-helios",
         )}
       >
         {latency}ms
       </p>
-      <svg
-        fill={color}
-        width={width}
-        height={height}
-        viewBox="0 0 32 32"
-        enableBackground="new 0 0 32 32"
-        id="Glyph"
-        version="1.1"
-        xmlSpace="preserve"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-      >
-        <path
-          d="M16,13c-1.654,0-3,1.346-3,3s1.346,3,3,3s3-1.346,3-3S17.654,13,16,13z"
-          id="XMLID_287_"
-        />
-        <path
-          d="M6,13c-1.654,0-3,1.346-3,3s1.346,3,3,3s3-1.346,3-3S7.654,13,6,13z"
-          id="XMLID_289_"
-        />
-        <path
-          d="M26,13c-1.654,0-3,1.346-3,3s1.346,3,3,3s3-1.346,3-3S27.654,13,26,13z"
-          id="XMLID_291_"
-        />
-      </svg>
+      <div className="flex size-6 items-center justify-center">
+        {isConnected && (
+          <svg
+            fill={color}
+            width={width}
+            height={height}
+            viewBox="0 0 32 32"
+            enableBackground="new 0 0 32 32"
+            id="Glyph"
+            version="1.1"
+            xmlSpace="preserve"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+          >
+            <path
+              d="M16,13c-1.654,0-3,1.346-3,3s1.346,3,3,3s3-1.346,3-3S17.654,13,16,13z"
+              id="XMLID_287_"
+            />
+            <path
+              d="M6,13c-1.654,0-3,1.346-3,3s1.346,3,3,3s3-1.346,3-3S7.654,13,6,13z"
+              id="XMLID_289_"
+            />
+            <path
+              d="M26,13c-1.654,0-3,1.346-3,3s1.346,3,3,3s3-1.346,3-3S27.654,13,26,13z"
+              id="XMLID_291_"
+            />
+          </svg>
+        )}
+        {!isConnected && (
+          <div className=" m-auto ">
+            <IoClose color={"#9C0534"} size={24} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
