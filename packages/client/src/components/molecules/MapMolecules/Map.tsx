eb615e4be6a661deaf0a568b255628ff0b49d607
David-Rodriguez-Barrios <david.rodriguezbarri@ucalgary.ca>
2024-07-20 14:09:06 -0600

    Initial draft for the centered button, and the satellite and dark mode switch button. Buttons resized, car animation between positions, and flag switch between satellite and street view.

--- packages/client/src/components/molecules/MapMolecules/Map.tsx
+++ packages/client/src/components/molecules/MapMolecules/Map.tsx
@@ -4,6 +4,14 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import ReactMapGL, { type MapLib, Marker } from "react-map-gl";
 
+import { useAppState } from "@/contexts/AppStateContext";
+import {
+  faGlobe,
+  faLayerGroup,
+  faLocationArrow,
+  faSatellite,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SportsScoreIcon from "@mui/icons-material/SportsScore";
 
 // @ts-expect-error:next-line
@@ -37,75 +45,115 @@ const fitToBounds = (
     maxZoom: 16,
   });
 };
+// linear interpolation for the animation for the car to catch up
+const lerp = (start: number, end: number, t: number) => {
+  return start * (1 - t) + end * t;
+};
 
-// class MapBoxControls{
-//   onAdd(map){
-//     this.
-//   }
-// }
 function Map(props: IMapProps): JSX.Element {
+  const { currentAppState } = useAppState();
   const { carLocation, mapLocation, lapLocation } = props;
   const mapRef = useRef<mapboxgl.Map | null>(null);
-  const [mapStyle, setMapStyle] = useState("mapbox://styles/mapbox/standard");
+  const [satelliteMode, setSatelliteMode] = useState(false);
+  const [centered, setCentered] = useState(false);
+  const buttonRef = useRef(null);
+  const [currentCarLocation, setCurrentCarLocation] = useState(carLocation);
+
   useEffect(() => {
-    const isOutsideBounds = (): boolean => {
+    const isOutsideBounds = (coordinates: ILocation[]): boolean => {
       if (!mapRef.current) return false;
-
-      const { lng, lat } = (
-        mapRef.current.getBounds() as LngLatBounds
-      ).getNorthEast();
-      const { lng: westLng, lat: southLat } = (
-        mapRef.current.getBounds() as LngLatBounds
-      ).getSouthWest();
-
-      return (
-        carLocation.lng < westLng ||
-        carLocation.lng > lng ||
-        carLocation.lat > lat ||
-        carLocation.lat < southLat ||
-        lapLocation.lng < westLng ||
-        lapLocation.lng > lng ||
-        lapLocation.lat > lat ||
-        lapLocation.lat < southLat
-      );
+      const bounds = mapRef.current?.getBounds() as LngLatBounds | undefined;
+      const { lng, lat } = bounds?.getNorthEast() || { lng: 0, lat: 0 };
+      const { lng: westLng, lat: southLat } = bounds?.getSouthWest() || {
+        lng: 0,
+        lat: 0,
+      };
+      for (
+        let i = 0;
+        coordinates !== undefined && i < coordinates.length;
+        i++
+      ) {
+        if (
+          coordinates[i]?.lng < westLng ||
+          coordinates[i]?.lng > lng ||
+          coordinates[i]?.lat < southLat ||
+          coordinates[i]?.lat > lat
+        ) {
+          return true;
+        }
+      }
+      return false;
     };
-
-    if (isOutsideBounds() && mapRef.current) {
+    const coordinates: ILocation[] = [carLocation, mapLocation, lapLocation];
+    if (isOutsideBounds(coordinates) && mapRef.current && !centered) {
       fitToBounds(mapRef.current, carLocation, lapLocation);
+    } else if (centered && mapRef.current) {
+      mapRef.current.flyTo({
+        center: [carLocation.lng, carLocation.lat],
+        zoom: 16,
+        speed: 1.5, // Adjust the speed of the animation
+        curve: 1, // Adjust the curve of the animation
+        easing: (t) => t, // Easing function for the animation
+      });
     }
-  }, [carLocation, lapLocation]);
+  }, [carLocation, lapLocation, centered]);
 
+  useEffect(() => {
+    let animationFrameId: number;
+    const animateCarMarker = () => {
+      setCurrentCarLocation((prevLocation) => {
+        const t = 0.01; // Animation speed [0,1]
+        const newLat = lerp(prevLocation.lat, carLocation.lat, t);
+        const newLng = lerp(prevLocation.lng, carLocation.lng, t);
+        return { lat: newLat, lng: newLng };
+      });
+      animationFrameId = requestAnimationFrame(animateCarMarker); // creates animation frame for the map
+    };
+    animateCarMarker();
+    return () => cancelAnimationFrame(animationFrameId);
+  }, [carLocation]);
   if (!process.env.NEXT_PUBLIC_MAPSAPIKEY) return <></>;
 
   const toggleMapStyle = () => {
-    setMapStyle((prevStyle) =>
-      prevStyle === "mapbox://styles/mapbox/dark-v11"
-        ? "mapbox://styles/mapbox/satellite-streets-v12"
-        : "mapbox://styles/mapbox/dark-v11",
-    );
-    console.log("MapStyle is now", mapStyle);
+    setSatelliteMode((prev) => !prev);
   };
+
   return (
     <>
-      <div style={{ width: "100%", height: "100%" }}>
-        <button
-          onClick={() => {
-            toggleMapStyle();
-          }}
-          style={{
-            position: "absolute",
-            top: "10px",
-            left: "10px",
-            zIndex: 1,
-            padding: "10px",
-            background: "white",
-            border: "none",
-            cursor: "pointer",
-          }}
-        >
-          Toggle Satellite
-        </button>
+      <div className="relative size-full">
+        <div className="absolute z-10 flex flex-col space-x-14 space-y-0 p-2">
+          <button
+            ref={buttonRef}
+            className="absolute z-10 flex size-8 cursor-pointer items-center justify-center rounded-full border-none bg-black"
+            onClick={() => {
+              toggleMapStyle();
+            }}
+          >
+            <FontAwesomeIcon
+              icon={satelliteMode === true ? faSatellite : faLayerGroup}
+              className="text-xl text-white"
+            />
+          </button>
+
+          <button
+            className="absolute z-10 flex size-8 cursor-pointer items-center justify-center rounded-full border-none bg-black"
+            onClick={() => {
+              setCentered(!centered);
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faLocationArrow}
+              style={{
+                color: "white",
+                fontSize: "0.3rem",
+                lineHeight: "0.3rem",
+                height: "1.5rem",
+              }}
+            />
+          </button>
+        </div>
         <ReactMapGL
+          ref={mapRef}
           mapLib={import("mapbox-gl") as Promise<MapLibType>}
           mapboxAccessToken={process.env.NEXT_PUBLIC_MAPSAPIKEY}
           initialViewState={{
@@ -114,7 +162,13 @@ function Map(props: IMapProps): JSX.Element {
             zoom: 14,
           }}
           style={{ width: "100%", height: "100%" }}
-          mapStyle={mapStyle}
+          mapStyle={
+            satelliteMode
+              ? "mapbox://styles/mapbox/satellite-streets-v12"
+              : currentAppState.darkMode
+                ? "mapbox://styles/mapbox/dark-v11"
+                : "mapbox://styles/mapbox/light-v11"
+          }
           boxZoom={false}
           doubleClickZoom={false}
           dragPan={true}
@@ -126,7 +180,10 @@ function Map(props: IMapProps): JSX.Element {
             fitToBounds(mapRef.current, carLocation, lapLocation);
           }}
         >
-          <Marker longitude={carLocation.lng} latitude={carLocation.lat}>
+          <Marker
+            longitude={currentCarLocation.lng}
+            latitude={currentCarLocation.lat}
+          >
             <Image
               src="/assets/HeliosBirdseye.png"
               alt="map-pin"
@@ -137,7 +194,14 @@ function Map(props: IMapProps): JSX.Element {
           <Marker
             longitude={lapLocation.lng}
             latitude={lapLocation.lat}
-            style={{ color: "white" }}
+            style={{
+              color:
+                satelliteMode === true
+                  ? "white"
+                  : currentAppState.darkMode === true
+                    ? "white"
+                    : "black",
+            }}
           >
             <SportsScoreIcon />
           </Marker>
