5ee3296c4dce2ee38eb805c7fca6a98c1dec85aa
alexwhelan12 <alexwh2005.12@gmail.com>
2024-07-22 18:08:40 -0600

    fixed cars default position

--- packages/client/src/components/molecules/HeroMolecules/CarMolecules/CarModelComponent.tsx
+++ packages/client/src/components/molecules/HeroMolecules/CarMolecules/CarModelComponent.tsx
@@ -113,7 +113,7 @@ export function CarModelComponent(props: CarModelComponentProps) {
       dispose={null}
       scale={3}
       rotation={[0, Math.PI, 0]}
-      position={[-1.5, 0, -2.5]}
+      position={[-2, 0, 6]}
     >
       <directionalLight
         name="Directional1"
