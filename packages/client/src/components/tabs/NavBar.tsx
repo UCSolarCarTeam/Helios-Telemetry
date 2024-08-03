ce0d92caaf03e0da378863b795e1d9755910ad63
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-08-03 15:00:07 -0600

    add Kris changes

--- packages/client/src/components/tabs/NavBar.tsx
+++ packages/client/src/components/tabs/NavBar.tsx
@@ -1,12 +1,11 @@
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
-import ScrollContainer from "react-indiana-drag-scroll";
 
 import { type SolarCarRoutes, routes } from "@/objects/TabRoutes";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 
-function NavBar(props: any): JSX.Element {
+function NavBar(): JSX.Element {
   const router = useRouter();
 
   const getCurrentPath = () => {
@@ -41,26 +40,37 @@ function NavBar(props: any): JSX.Element {
   };
 
   return (
-    <ScrollContainer className="w-full">
-      <Tabs
-        className="w-full"
-        value={value}
-        variant="fullWidth"
-        onChange={handleChange}
-        TabIndicatorProps={{ style: { backgroundColor: "#B94A6C" } }}
-        sx={{ minHeight: "4vh" }}
-      >
-        {routes.map((route: SolarCarRoutes, i: number) => (
-          <Tab
-            sx={{ minHeight: "4vh", height: "4vh" }}
-            disableRipple
-            key={i}
-            value={route.value}
-            label={<h3 className="text-light dark:text-dark">{route.id}</h3>}
-          ></Tab>
-        ))}
-      </Tabs>
-    </ScrollContainer>
+    <Tabs
+      className="w-full"
+      value={value}
+      onChange={handleChange}
+      variant="scrollable" // make scrollable
+      scrollButtons // show scroll buttons
+      selectionFollowsFocus // allows use of arrow keys
+      allowScrollButtonsMobile // shows scroll buttons on mobile
+      aria-label="scrollable tabs"
+      TabIndicatorProps={{ style: { backgroundColor: "#B94A6C" } }} // indicate what tab you are on
+      sx={{
+        minHeight: "4vh",
+        display: "flex",
+        "& .MuiTabs-flexContainer": {
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexWrap: "nowrap",
+        },
+      }}
+    >
+      {routes.map((route: SolarCarRoutes, i: number) => (
+        <Tab
+          sx={{ minHeight: "4vh", height: "4vh" }}
+          disableRipple
+          key={i}
+          value={route.value}
+          label={<h3 className="text-light dark:text-dark">{route.id}</h3>}
+        ></Tab>
+      ))}
+    </Tabs>
   );
 }
 
