6dcb6cdd6dca40319fb3141e5190353e35a986a1
Burton Jong <burtonjong05@gmail.com>
2024-09-23 18:01:23 -0600

    reverse road

--- packages/client/src/components/molecules/HeroMolecules/CarMolecules/RoadComponent.tsx
+++ packages/client/src/components/molecules/HeroMolecules/CarMolecules/RoadComponent.tsx
@@ -1,16 +1,21 @@
 /* eslint-disable react/no-unknown-property */
-import { memo, useRef } from "react";
+import { memo, useMemo, useRef } from "react";
 import * as THREE from "three";
 import { PlaneGeometry } from "three";
 
+import { type IDriverControls } from "@/objects/telemetry-data.interface";
 import { type Vector3, useFrame } from "@react-three/fiber";
 
 interface roadComponentProps {
   size: number;
+  direction: IDriverControls;
   speed: number;
 }
 const RoadComponent = (props: roadComponentProps) => {
-  const geometry = new PlaneGeometry(0.5, props.size * 5);
+  const geometry = useMemo(
+    () => new PlaneGeometry(0.5, props.size * 5),
+    [props.size],
+  );
   const planeGeom = new PlaneGeometry(9, props.size * 7);
   return (
     <>
@@ -29,22 +34,36 @@ const RoadComponent = (props: roadComponentProps) => {
 interface roadStripComponentProps {
   size: number;
   speed: number;
+  direction: IDriverControls;
   position: Vector3;
 }
 function RoadStripComponent(props: roadStripComponentProps) {
   const mesh = useRef<THREE.Mesh>(null!);
-  const geometry = new THREE.PlaneGeometry(0.5, 3);
-
+  const geometry = useMemo(() => new THREE.PlaneGeometry(0.5, 3), []);
   useFrame((_, delta) => {
     if (mesh.current == null) {
       return;
     }
 
-    mesh.current.position.z -= delta * props.speed;
-    const back = (props.size / 2) * -5 - 5;
-    const front = props.size * 5;
-    if (mesh.current.position.z < back) {
-      mesh.current.position.z += front;
+    if (props.direction.Forward) {
+      mesh.current.position.z -= delta * props.speed;
+    } else {
+      mesh.current.position.z += delta * props.speed;
+    }
+
+    const stripLength = 5; // Length of each road strip
+    const totalLength = props.size * stripLength;
+    const back = (props.size / 2) * -stripLength - stripLength;
+    const front = (props.size / 2) * stripLength + stripLength;
+
+    if (props.direction.Forward) {
+      if (mesh.current.position.z < back) {
+        mesh.current.position.z += totalLength;
+      }
+    } else {
+      if (mesh.current.position.z > front) {
+        mesh.current.position.z -= totalLength;
+      }
     }
   });
 
@@ -62,17 +81,20 @@ function RoadStripComponent(props: roadStripComponentProps) {
 
 function RoadStripGroup(props: roadComponentProps) {
   const roadStrips: number[] = [];
+
   for (let i = 0; i < props.size; i++) {
     const back = (props.size / 2) * -5;
     roadStrips.push(back + i * 5);
   }
 
+  console.log(roadStrips);
   return (
     <>
       {roadStrips.map((z, i) => (
         <RoadStripComponent
           key={i}
-          position={[-5.5, -0.01, z]}
+          position={[-5.5, -0.1, z]}
+          direction={props.direction}
           size={props.size}
           speed={props.speed}
         />
