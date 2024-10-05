9d7752ae9bbc498b6ef96a52080cea45578c5383
brian-ngyn <brian.nguy1en@gmail.com>
2024-10-05 11:58:10 -0600

    FEATURE: Add Dynamic Lap marker
    Co-authored-by: Burton Jong <burtonjong05@gmail.com>
    Co-authored-by: justin-phxm <113923596+justin-phxm@users.noreply.github.com>

--- packages/client/src/components/molecules/LogoStatusMolecules/SettingsComponent.tsx
+++ packages/client/src/components/molecules/LogoStatusMolecules/SettingsComponent.tsx
@@ -1,53 +1,109 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import {
   APPUNITS,
   CONNECTIONTYPES,
   useAppState,
 } from "@/contexts/AppStateContext";
+import { socketIO } from "@/contexts/SocketContext";
 import SettingsIcon from "@mui/icons-material/Settings";
+import { Button, TextField } from "@mui/material";
 import Modal from "@mui/material/Modal";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { CoordInfoUpdate, CoordUpdateResponse } from "@shared/helios-types";
 
+const coordsFieldText: CoordInfoUpdate = {
+  lat: "Latitude",
+  long: "Longitude",
+  password: "Password",
+};
 function SettingsComponent() {
   const { currentAppState, setCurrentAppState } = useAppState();
   const [open, setOpen] = useState(false);
-  const handleDarkChange = (
-    event: React.MouseEvent<HTMLElement>,
-    inputMode: (typeof currentAppState)["darkMode"],
-  ) => {
-    if (inputMode !== null) {
-      setCurrentAppState((prev) => ({
-        ...prev,
-        darkMode: inputMode,
-      }));
+  const [coords, setCoords] = useState<CoordInfoUpdate>({
+    lat: currentAppState.lapCoords.lat.toString(),
+    long: currentAppState.lapCoords.long.toString(),
+    password: "",
+  });
+  const [errors, setErrors] = useState<Set<keyof CoordInfoUpdate>>(new Set());
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const onLapCoords = useCallback((coords: CoordUpdateResponse) => {
+    if ("invalidFields" in coords && coords.invalidFields) {
+      const errorSet: Set<keyof CoordInfoUpdate> = new Set(
+        coords.invalidFields,
+      );
+      setErrors(errorSet);
+      setErrorMessage(coords.error ?? "");
+    } else {
+      setErrors(new Set());
     }
-  };
+  }, []);
+  useEffect(() => {
+    socketIO.on("lapCoords", onLapCoords);
+    () => {
+      socketIO.off("lapCoords");
+    };
+  }, [onLapCoords]);
+  const handleCoordsSubmit = useCallback(() => {
+    const newCoordInfo = {
+      lat: coords.lat,
+      long: coords.long,
+      password: coords.password,
+    };
+    socketIO.emit("setLapCoords", newCoordInfo);
+  }, [coords]);
+  const handleCoordsChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = event.target;
+      setCoords((prev) => ({
+        ...prev,
+        [name]: value ? value : prev[name as keyof CoordInfoUpdate],
+      }));
+    },
+    [],
+  );
+  const handleDarkChange = useCallback(
+    (
+      event: React.MouseEvent<HTMLElement>,
+      inputMode: (typeof currentAppState)["darkMode"],
+    ) => {
+      if (inputMode !== null) {
+        setCurrentAppState((prev) => ({
+          ...prev,
+          darkMode: inputMode,
+        }));
+      }
+    },
+    [setCurrentAppState],
+  );
 
-  const handleUnitChange = (
-    event: React.MouseEvent<HTMLElement>,
-    inputMode: (typeof currentAppState)["appUnits"],
-  ) => {
-    if (inputMode !== null) {
-      setCurrentAppState((prev) => ({
-        ...prev,
-        appUnits: inputMode,
-      }));
-    }
-  };
+  const handleUnitChange = useCallback(
+    (
+      event: React.MouseEvent<HTMLElement>,
+      inputMode: (typeof currentAppState)["appUnits"],
+    ) => {
+      if (inputMode !== null) {
+        setCurrentAppState((prev) => ({
+          ...prev,
+          appUnits: inputMode,
+        }));
+      }
+    },
+    [setCurrentAppState],
+  );
 
-  const handleConnectionChange = (
-    event: React.MouseEvent<HTMLElement>,
-    inputMode: CONNECTIONTYPES,
-  ) => {
-    if (inputMode !== null) {
-      setCurrentAppState((prev) => ({
-        ...prev,
-        connectionType: inputMode,
-      }));
-    }
-  };
+  const handleConnectionChange = useCallback(
+    (event: React.MouseEvent<HTMLElement>, inputMode: CONNECTIONTYPES) => {
+      if (inputMode !== null) {
+        setCurrentAppState((prev) => ({
+          ...prev,
+          connectionType: inputMode,
+        }));
+      }
+    },
+    [setCurrentAppState],
+  );
   return (
     <div className="grid">
       <h2
@@ -179,6 +235,41 @@ function SettingsComponent() {
                 )}
               </div>
             </div>
+          </div>
+
+          <div className="mb-4 grid grid-cols-2 items-center justify-between">
+            <div className="col-span-1">
+              <label className="mr-2">Update Flag Coordinates:</label>
+            </div>
+            <form
+              className="col-span-1 flex flex-col"
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              {Object.keys(coordsFieldText).map((key) => (
+                <TextField
+                  error={errors.has(key as keyof CoordInfoUpdate)}
+                  helperText={
+                    errors.has(key as keyof CoordInfoUpdate) ? errorMessage : ""
+                  }
+                  key={key}
+                  label={coordsFieldText[key as keyof CoordInfoUpdate]}
+                  name={key}
+                  onChange={handleCoordsChange}
+                  placeholder={
+                    key !== "password"
+                      ? coords[key as keyof CoordInfoUpdate]
+                      : ""
+                  }
+                  type={key === "password" ? "password" : undefined}
+                  variant="filled"
+                />
+              ))}
+              <Button onClick={handleCoordsSubmit} type="submit">
+                Submit
+              </Button>
+            </form>
           </div>
         </div>
       </Modal>
