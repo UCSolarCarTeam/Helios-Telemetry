438cb2a4b9906bf72ba9c0e26be38a5c336e5aa4
alexwhelan12 <alexwh2005.12@gmail.com>
2024-10-19 11:42:11 -0600

    changed function to useCallback

--- packages/client/src/components/containers/BottomInformationContainer.tsx
+++ packages/client/src/components/containers/BottomInformationContainer.tsx
@@ -1,4 +1,5 @@
 import isEqual from "lodash/isEqual";
+import { useCallback } from "react";
 
 import { useAppState } from "@/contexts/AppStateContext";
 import usePIS from "@/hooks/PIS/usePIS";
@@ -10,23 +11,38 @@ function BottomInformationContainer() {
   const { battery, motor, mppt } = usePIS();
   const favourites = currentAppState?.favourites ?? [];
 
-  const findValueByNameInData = (
-    dataArray: I_PIS[],
-    providedName: string,
-  ): string | number | undefined => {
-    //Loop through each object in the dataArray
-    dataArray = dataArray.filter((data) => data !== undefined);
-    for (const data of dataArray) {
-      if (isEqual(data, mppt)) {
-        // Loop through each Unit key (like Unit0, Unit1, etc.)
-        for (const unitKey of Object.keys(data)) {
-          const unit = data[unitKey] as Record<string, I_PISField[]>; // Get the object under each Unit key
-          // Loop through each Channel key within the Unit (like Channel0, Channel1, etc.)
-          if (unit === undefined) continue;
-          for (const channelKey of Object.keys(unit)) {
-            const channel = unit[channelKey] as I_PISField[];
+  const findValueByNameInData = useCallback(
+    (dataArray: I_PIS[], providedName: string): string | number | undefined => {
+      // Loop through each object in the dataArray
+      dataArray = dataArray.filter((data) => data !== undefined);
+      for (const data of dataArray) {
+        if (isEqual(data, mppt)) {
+          // Loop through each Unit key (like Unit0, Unit1, etc.)
+          for (const unitKey of Object.keys(data)) {
+            const unit = data[unitKey] as Record<string, I_PISField[]>; // Get the object under each Unit key
+            // Loop through each Channel key within the Unit (like Channel0, Channel1, etc.)
+            if (unit === undefined) continue;
+            for (const channelKey of Object.keys(unit)) {
+              const channel = unit[channelKey] as I_PISField[];
+              // Loop through each object in the array
+              for (const item of channel) {
+                // Check if the name matches the providedName
+                if (item.name === providedName) {
+                  // Return the value inside the `data` array of the matching object
+                  if (typeof item.data[0]?.value === "boolean") {
+                    return item.data[0]?.value ? "T" : "F";
+                  }
+                  return item.data[0]?.value;
+                }
+              }
+            }
+          }
+          return undefined; // Return undefined if no matching name is found
+        } else {
+          for (const sectionKey of Object.keys(data)) {
+            const section = data[sectionKey] as I_PISField[]; // Get the array of items under each key
             // Loop through each object in the array
-            for (const item of channel) {
+            for (const item of section) {
               // Check if the name matches the providedName
               if (item.name === providedName) {
                 // Return the value inside the `data` array of the matching object
@@ -38,26 +54,11 @@ function BottomInformationContainer() {
             }
           }
         }
-        return undefined; // Return undefined if no matching name is found
-      } else {
-        for (const sectionKey of Object.keys(data)) {
-          const section = data[sectionKey] as I_PISField[]; // Get the array of items under each key
-          // Loop through each object in the array
-          for (const item of section) {
-            // Check if the name matches the providedName
-            if (item.name === providedName) {
-              // Return the value inside the `data` array of the matching object
-              if (typeof item.data[0]?.value === "boolean") {
-                return item.data[0]?.value ? "T" : "F";
-              }
-              return item.data[0]?.value;
-            }
-          }
-        }
       }
-    }
-    return undefined; // Return undefined if no matching name is found
-  };
+      return undefined; // Return undefined if no matching name is found
+    },
+    [],
+  );
 
   return (
     <div className="align-middle">
