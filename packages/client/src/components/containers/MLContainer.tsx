8334e4830f6560b9b526f25f11677d9b7ef626a4
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-12-03 20:14:37 -0700

    add lambda support

--- packages/client/src/components/containers/MLContainer.tsx
+++ packages/client/src/components/containers/MLContainer.tsx
@@ -1,35 +1,40 @@
 "use client";
 
-import axios from "axios";
 import dynamic from "next/dynamic";
 import { useCallback, useEffect, useState } from "react";
 import { PlotParams } from "react-plotly.js";
 
-import correlationMatrixData from "@/correlationMatrixData.json";
 import useWindowDimensions from "@/hooks/PIS/useWindowDimensions";
 
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 const SMALL_SCREEN = 380;
-export default function MLContainer() {
-  const [plot, setPlot] = useState<PlotParams>();
+type PlotTypes =
+  | "/api/getPacketCorrelationMatrix"
+  | "/api/getLapCorrelationMatrix";
+export default function MLContainer({
+  plotType = "/api/getLapCorrelationMatrix",
+}: {
+  plotType?: PlotTypes;
+}) {
+  const [plot, setPlot] = useState<PlotParams | { error: boolean }>();
   const { width } = useWindowDimensions();
 
   const fetchPlot = useCallback(async () => {
-    let data = JSON.parse(correlationMatrixData);
-
     try {
-      const response = await axios.get("http://localhost:8000/packet_plot");
-      data = JSON.parse(await response.data);
-    } catch {}
+      const response = await fetch(plotType);
+      const graph = await response.json();
+      const data = JSON.parse(graph);
+      const layout: PlotParams["layout"] = {
+        autosize: true,
+        title: data.layout.title.text,
+      };
+      data.layout = layout;
 
-    const layout: PlotParams["layout"] = {
-      autosize: true,
-      title: data.layout.title.text,
-    };
-    data.layout = layout;
-
-    setPlot(data);
-  }, []);
+      setPlot(data);
+    } catch (e) {
+      setPlot({ error: true });
+    }
+  }, [plotType]);
 
   useEffect(() => {
     fetchPlot();
@@ -38,26 +43,27 @@ export default function MLContainer() {
   if (!plot) {
     return <div>Loading...</div>;
   }
+  if ("error" in plot) {
+    return <div>Error loading data</div>;
+  }
   return (
-    <div className="flex max-h-96 w-full items-center justify-center gap-4 rounded-lg bg-white p-2 text-3xl font-bold">
-      <Plot
-        className="h-96 w-full"
-        config={{
-          displayModeBar: width < SMALL_SCREEN,
-          displaylogo: false,
-          modeBarButtonsToRemove: [
-            "toImage",
-            "zoomOut2d",
-            "zoom2d",
-            "resetScale2d",
-          ],
-          scrollZoom: true,
-          staticPlot: width < SMALL_SCREEN,
-        }}
-        data={plot.data}
-        layout={plot.layout}
-        useResizeHandler={true}
-      />
-    </div>
+    <Plot
+      className="h-96 w-full"
+      config={{
+        displayModeBar: width < SMALL_SCREEN,
+        displaylogo: false,
+        modeBarButtonsToRemove: [
+          "toImage",
+          "zoomOut2d",
+          "zoom2d",
+          "resetScale2d",
+        ],
+        scrollZoom: true,
+        staticPlot: width < SMALL_SCREEN,
+      }}
+      data={plot.data}
+      layout={plot.layout}
+      useResizeHandler={true}
+    />
   );
 }
