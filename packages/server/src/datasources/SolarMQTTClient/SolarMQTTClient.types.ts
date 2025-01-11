7e3e8c73c4d518f1e81ce21fdf0ebdced7437145
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2025-01-11 16:43:31 -0700

    moved critical server env vars to aws secrets manager

--- packages/server/src/datasources/SolarMQTTClient/SolarMQTTClient.types.ts
+++ packages/server/src/datasources/SolarMQTTClient/SolarMQTTClient.types.ts
@@ -2,12 +2,13 @@ import type { MqttClient } from "mqtt";
 import type { IClientOptions } from "mqtt/*";
 
 export const options: IClientOptions = {
-  host: "aedes.calgarysolarcar.ca",
-  password: process.env.MQTT_PASSWORD,
+  host:
+    process.env.NODE_ENV === "development"
+      ? "localhost"
+      : "aedes.calgarysolarcar.ca",
   port: 1883,
   protocol: "tcp",
   protocolVersion: 3,
-  username: process.env.MQTT_USERNAME,
 };
 
 export const topics = {
