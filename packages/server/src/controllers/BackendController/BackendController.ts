098cba883ac2cf6fd41cb305d3a8e7e905d48eff
Kris <78143563+kr1s-r@users.noreply.github.com>
2025-01-25 16:21:21 -0700

    implementing sending and receiving data

--- packages/server/src/controllers/BackendController/BackendController.ts
+++ packages/server/src/controllers/BackendController/BackendController.ts
@@ -16,31 +16,36 @@ export class BackendController implements BackendControllerTypes {
   public socketIO: SocketIO;
   public lapController: LapController;
   public mqtt: SolarMQTTClient;
+  public carLatency: number;
   constructor(
     httpsServer: Server<typeof IncomingMessage, typeof ServerResponse>,
   ) {
-    // this.dynamoDB = new DynamoDB(this);
+    this.dynamoDB = new DynamoDB(this);
     this.socketIO = new SocketIO(httpsServer, this);
     this.mqtt = new SolarMQTTClient(options, this);
     this.lapController = new LapController(this);
     this.establishCarPinging();
+    this.carLatency = 0;
     // this.handleCarLatency();
   }
 
   public establishCarPinging() {
     // Ping the car every 5 seconds
     this.mqtt.pingTimer(5000);
+
+    // send data to car every 5 seconds
+    this.mqtt.telemetryToCar(5000);
   }
 
-  public handleCarLatency(carLatency: number) {
+  public handleTelemetryToCar(carLatency: number) {
     // Broadcast the car latency to the frontend
     this.socketIO.broadcastCarLatency(carLatency);
-    logger.info("Car Latency: ", carLatency.toString());
+    logger.info("Car Latency - receiving: ", carLatency.toString());
   }
 
   public async handlePacketReceive(message: ITelemetryData) {
     // Insert the packet into the database
-    // this.dynamoDB.insertPacketData(message);
+    this.dynamoDB.insertPacketData(message);
 
     // Broadcast the packet to the frontend
     this.socketIO.broadcastPacket(message);
