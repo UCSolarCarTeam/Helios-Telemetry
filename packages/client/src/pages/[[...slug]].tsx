a255ea0438d0a2265b2abdaf4f757151c0d99f5b
burtonjong <burtonjong05@gmail.com>
2025-02-08 13:55:13 -0700

    datepicker component

--- packages/client/src/pages/[[...slug]].tsx
+++ packages/client/src/pages/[[...slug]].tsx
@@ -30,7 +30,6 @@ export default function Home() {
             </div>
           </div>
           <div className="grid">
-            <BottomInformationContainer />
             {currentAppState.playbackSwitch ? (
               <PlaybackSlider />
             ) : (
