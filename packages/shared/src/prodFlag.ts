c56d8da0e61b5a1a3a636091a74dd4c39f1ac97e
burtonjong <burtonjong05@gmail.com>
2025-02-11 09:12:38 -0700

    make the endpoint localhost or aedes based on development environment

---
+++ packages/shared/src/prodFlag.ts
@@ -0,0 +1,9 @@
+export const prodFlag =
+  process.env.NODE_ENV === "production"
+    ? "aedes.calgarysolarcar.ca:3001"
+    : "http://localhost:3001";
+
+export const prodURL =
+  process.env.NODE_ENV === "production"
+    ? "https://aedes.calgarysolarcar.ca:3001"
+    : "http://localhost:3001";
