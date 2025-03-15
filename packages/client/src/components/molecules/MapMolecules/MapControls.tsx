0e8e088ec97e4bf7595b3346f621002b99807495
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2025-03-15 14:40:27 -0600

    change hover to focus-within

--- packages/client/src/components/molecules/MapMolecules/MapControls.tsx
+++ packages/client/src/components/molecules/MapMolecules/MapControls.tsx
@@ -48,18 +48,18 @@ export default function MapControls({
       >
         <FaLocationArrow className={iconClasses} />
       </button>
-      <div
+      <button
         className={twMerge(
           buttonClasses,
-          "group relative transition-all duration-700 hover:h-24 hover:w-32 hover:rounded-md",
+          "group relative transition-all duration-700 focus-within:h-24 focus-within:w-32 focus-within:rounded-md",
         )}
       >
-        <FaMagnifyingGlass className="transition duration-200 group-hover:opacity-0" />
+        <FaMagnifyingGlass className="transition duration-200 group-focus-within:opacity-0" />
         <div className="group absolute bottom-0 left-2 top-1.5 flex flex-col gap-2">
           {trackList.map((track, index) => {
             return (
               <button
-                className="flex size-full -translate-y-10 items-center gap-2 text-nowrap text-end opacity-0 transition duration-500 group-hover:translate-y-0 group-hover:opacity-100"
+                className="flex size-full -translate-y-10 items-center gap-2 text-nowrap text-end opacity-0 transition duration-500 group-focus-within:translate-y-0 group-focus-within:opacity-100"
                 key={track.sourceProps.id}
                 onClick={() =>
                   setViewTracks((prevState) => {
@@ -82,14 +82,14 @@ export default function MapControls({
                         : "transparent",
                   }}
                 />
-                <p className="hidden select-none text-xxs font-semibold group-hover:block">
+                <p className="hidden select-none text-xxs font-semibold group-focus-within:block">
                   {track.trackName}
                 </p>
               </button>
             );
           })}
         </div>
-      </div>
+      </button>
     </div>
   );
 }
