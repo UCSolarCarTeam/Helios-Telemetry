a74c1a3d5baf3f34a2cb69c524c6484c87062b7f
sim <simarkandola51@gmail.com>
2025-03-27 16:41:56 -0600

    fixed flags

--- packages/client/src/components/molecules/MapMolecules/PacketMarker.tsx
+++ packages/client/src/components/molecules/MapMolecules/PacketMarker.tsx
@@ -1,4 +1,5 @@
-import { Marker, Popup } from "react-map-gl";
+import type { FeatureCollection } from "geojson";
+import { Layer, LayerProps, Marker, Popup, Source } from "react-map-gl";
 
 import SportsScoreIcon from "@mui/icons-material/SportsScore";
 
@@ -14,6 +15,36 @@ export default function PacketMarker(props: {
   const { index, onMouseEnterDataPoint, onMouseLeaveDataPoint, packetMarker } =
     props;
   const { data, markerCoords, open } = packetMarker;
+
+  const geojson: FeatureCollection = {
+    features: [
+      {
+        geometry: {
+          coordinates: [
+            packetMarker.markerCoords.longitude,
+            packetMarker.markerCoords.latitude,
+          ],
+          type: "Point",
+        },
+        properties: { title: "Finish Line !!!" },
+        type: "Feature",
+      },
+    ],
+    type: "FeatureCollection",
+  };
+
+  const layerStyle: LayerProps = {
+    id: "finish-line",
+    paint: {
+      "circle-color": "#B94A6C",
+      "circle-opacity": 0.8,
+      "circle-radius": 20,
+      "circle-stroke-color": "#9C0534",
+      "circle-stroke-width": 2,
+    },
+    type: "circle",
+  };
+
   return (
     <>
       {open && <Popup {...markerCoords}>{data.TimeStamp}</Popup>}
@@ -24,6 +55,9 @@ export default function PacketMarker(props: {
           onMouseLeave={() => onMouseLeaveDataPoint(index)}
         />
       </Marker>
+      <Source data={geojson} id="finish-line-source" type="geojson">
+        <Layer {...layerStyle} />
+      </Source>
     </>
   );
 }
