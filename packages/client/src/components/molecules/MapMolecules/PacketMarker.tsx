b3794206c859e5931f70c79bb4f01fdb2de3a00d
sim <simarkandola51@gmail.com>
2025-03-29 13:18:13 -0600

    dots instead of flags

--- packages/client/src/components/molecules/MapMolecules/PacketMarker.tsx
+++ packages/client/src/components/molecules/MapMolecules/PacketMarker.tsx
@@ -1,8 +1,7 @@
-import type { FeatureCollection } from "geojson";
 import { JSX } from "react";
-import { Layer, LayerProps, Marker, Popup, Source } from "react-map-gl";
+import { Marker, Popup } from "react-map-gl";
 
-import SportsScoreIcon from "@mui/icons-material/SportsScore";
+import PanoramaFishEyeIcon from "@mui/icons-material/PanoramaFishEye";
 
 import type { PacketMarkerData } from "./Map";
 
@@ -17,48 +16,19 @@ export default function PacketMarker(props: {
     props;
   const { data, markerCoords, open } = packetMarker;
 
-  const geojson: FeatureCollection = {
-    features: [
-      {
-        geometry: {
-          coordinates: [
-            packetMarker.markerCoords.longitude,
-            packetMarker.markerCoords.latitude,
-          ],
-          type: "Point",
-        },
-        properties: { title: "Finish Line !!!" },
-        type: "Feature",
-      },
-    ],
-    type: "FeatureCollection",
-  };
-
-  const layerStyle: LayerProps = {
-    id: "finish-line",
-    paint: {
-      "circle-color": "#B94A6C",
-      "circle-opacity": 0.8,
-      "circle-radius": 20,
-      "circle-stroke-color": "#9C0534",
-      "circle-stroke-width": 2,
-    },
-    type: "circle",
-  };
-
   return (
     <>
       {open && <Popup {...markerCoords}>{data.TimeStamp}</Popup>}
-      <Marker {...markerCoords}>
-        <SportsScoreIcon
-          height={50}
+      <Marker
+        latitude={packetMarker.markerCoords.latitude}
+        longitude={packetMarker.markerCoords.longitude}
+      >
+        <PanoramaFishEyeIcon
+          fontSize="10px"
           onMouseEnter={() => onMouseEnterDataPoint(index)}
           onMouseLeave={() => onMouseLeaveDataPoint(index)}
         />
       </Marker>
-      <Source data={geojson} id="finish-line-source" type="geojson">
-        <Layer {...layerStyle} />
-      </Source>
     </>
   );
 }
