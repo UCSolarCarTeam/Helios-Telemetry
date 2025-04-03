60c5d450638d0d72e37dbb9bc4eb1605f1081433
alexwhelan12 <alexwh2005.12@gmail.com>
2025-04-03 17:52:28 -0600

    contiunued playback feature, still need to finish integrating with slider component

--- packages/client/src/pages/[[...slug]].tsx
+++ packages/client/src/pages/[[...slug]].tsx
@@ -5,6 +5,7 @@ import MapContainer from "@/components/containers/MapContainer";
 import TabsContainer from "@/components/containers/TabsContainer";
 import PlaybackSlider from "@/components/molecules/PlaybackMolecules/PlaybackSlider";
 import { useAppState } from "@/contexts/AppStateContext";
+import { PlaybackContextProvider } from "@/contexts/PlayBackContext";
 
 export default function Home() {
   const { currentAppState } = useAppState();
@@ -12,31 +13,33 @@ export default function Home() {
   return (
     <div className={currentAppState.darkMode ? "dark" : ""}>
       <div className="h-full w-screen overflow-x-hidden bg-light p-4 text-light dark:bg-dark dark:text-dark lg:h-screen">
-        <div className="flex h-full flex-col gap-y-2">
-          <div className="flex h-full flex-wrap gap-2">
-            <div className="mx-auto w-36">
-              <LogoStatusContainer />
+        <PlaybackContextProvider>
+          <div className="flex h-full flex-col gap-y-2">
+            <div className="flex h-full flex-wrap gap-2">
+              <div className="mx-auto w-36">
+                <LogoStatusContainer />
+              </div>
+              <div className="w-full flex-1 flex-row">
+                <TabsContainer />
+              </div>
             </div>
-            <div className="w-full flex-1 flex-row">
-              <TabsContainer />
+            <div className="flex flex-row flex-wrap gap-2">
+              <div className="size-96 grow">
+                <MapContainer />
+              </div>
+              <div className="grow basis-2/3">
+                <HeroContainer />
+              </div>
+            </div>
+            <div className="grid">
+              {currentAppState.playbackSwitch ? (
+                <PlaybackSlider />
+              ) : (
+                <BottomInformationContainer />
+              )}
             </div>
           </div>
-          <div className="flex flex-row flex-wrap gap-2">
-            <div className="size-96 grow">
-              <MapContainer />
-            </div>
-            <div className="grow basis-2/3">
-              <HeroContainer />
-            </div>
-          </div>
-          <div className="grid">
-            {currentAppState.playbackSwitch ? (
-              <PlaybackSlider />
-            ) : (
-              <BottomInformationContainer />
-            )}
-          </div>
-        </div>
+        </PlaybackContextProvider>
       </div>
     </div>
   );
