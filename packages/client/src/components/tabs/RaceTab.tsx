ce8eab1b3aeca3a5c420aa3fd5d817e732f3fcf7
promatty <matthewlblue@gmail.com>
2025-05-17 14:43:05 -0600

    better tailwind classes

--- packages/client/src/components/tabs/RaceTab.tsx
+++ packages/client/src/components/tabs/RaceTab.tsx
@@ -361,7 +361,7 @@ function RaceTab() {
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
                   <th
-                    className={`sticky top-0 z-10 w-24 border-b-2 border-r-2 border-t-2 border-helios px-4 py-2 text-center text-xs font-medium uppercase text-helios first:border-l-2 ${header.id === "data_timeStamp" ? "left-0 z-50" : ""} ${currentAppState.darkMode ? "bg-lightergrey" : "bg-slate"}`}
+                    className={`sticky top-0 z-10 w-24 border-b-2 border-r-2 border-t-2 border-helios bg-slate px-4 py-2 text-center text-xs font-medium uppercase text-helios first:border-l-2 dark:bg-lightergrey ${header.id === "data_timeStamp" ? "left-0 z-50" : ""}`}
                     key={header.id}
                   >
                     {header.isPlaceholder
@@ -389,12 +389,12 @@ function RaceTab() {
           <tbody className="divide-y divide-gray-200">
             {table.getRowModel().rows.map((row) => (
               <tr
-                className={`${currentAppState.darkMode ? "odd:bg-darkergrey even:bg-lightergrey" : "odd:bg-white even:bg-slate"}`}
+                className="odd:bg-white even:bg-slate dark:odd:bg-darkergrey dark:even:bg-lightergrey"
                 key={row.id}
               >
                 {row.getVisibleCells().map((cell) => (
                   <td
-                    className={`w-fullpx-4 sticky w-24 border-b-2 border-r-2 border-helios py-2 text-center text-sm first:border-l-2 ${cell.id.includes("data_timeStamp") ? "left-0 z-10" : ""} ${currentAppState.darkMode ? "text-white" : "text-gray-900"}`}
+                    className={`text-gray-900 w-fullpx-4 sticky w-24 border-b-2 border-r-2 border-helios py-2 text-center text-sm first:border-l-2 dark:text-white ${cell.id.includes("data_timeStamp") ? "left-0 z-10" : ""}`}
                     key={cell.id}
                   >
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
