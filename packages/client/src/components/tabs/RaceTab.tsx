c4cd181a6f37043b163812ef883477b3d6f6a9b2
burtonjong <burtonjong05@gmail.com>
2025-05-24 11:32:00 -0600

    fix

--- packages/client/src/components/tabs/RaceTab.tsx
+++ packages/client/src/components/tabs/RaceTab.tsx
@@ -33,7 +33,6 @@ import {
  * When the column filter is changed, the setColumnName() is called to set the column name
  */
 function RaceTab() {
-  const { currentAppState } = useAppState();
   const [Rfid, setDriverRFID] = useState<number | string>("");
   const [driverData, setDriverData] = useState<IDriverData[]>([]);
   const [copy, setCopy] = useState<number>(0);
