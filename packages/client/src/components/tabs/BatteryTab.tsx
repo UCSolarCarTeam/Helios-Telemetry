f303efb9d201a59cecd81514ab0e3800b57dfc1f
promatty <matthewlblue@gmail.com>
2025-05-31 14:22:10 -0600

    wip: fullscreen wrapper for tabs
    Created a wrapper to make other components fullscreen. Fullscreen functionality doesn't work yet

--- packages/client/src/components/tabs/BatteryTab.tsx
+++ packages/client/src/components/tabs/BatteryTab.tsx
@@ -1,4 +1,5 @@
 import PISTransformer from "@/components/transformers/PISTransformer";
+import FullscreenWrapper from "@/contexts/FullscreenWrapper";
 import usePIS from "@/hooks/PIS/usePIS";
 import type I_PIS from "@/objects/PIS/PIS.interface";
 
@@ -7,7 +8,9 @@ function BatteryTab() {
 
   return (
     <div>
-      <PISTransformer root={battery as I_PIS} />
+      <FullscreenWrapper>
+        <PISTransformer root={battery as I_PIS} />
+      </FullscreenWrapper>
     </div>
   );
 }
