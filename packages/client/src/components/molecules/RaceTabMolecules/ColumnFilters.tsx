0869c875011e144e20b36c938da9abaf9844b349
Burton Jong <108391733+burtonjong@users.noreply.github.com>
2025-05-31 14:11:55 -0600

    Tel 257 fix hover animations on laps table (#237)
    * edit docs, refactor race tab
    
    * done
    
    * fix
    
    * remove package
    
    * readd dark mode styles

---
+++ packages/client/src/components/molecules/RaceTabMolecules/ColumnFilters.tsx
@@ -0,0 +1,120 @@
+import { useCallback } from "react";
+
+import { useAppState } from "@/contexts/AppStateContext";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { IFormattedLapData } from "@shared/helios-types";
+import { Table } from "@tanstack/react-table";
+
+/*
+ * This component is used to filter the columns of the lap data table
+ * Contains the main functionality of the column picker
+ */
+export default function ColumnFilters({
+  columnName,
+  setColumnName,
+  table,
+}: {
+  columnName: string[];
+  setColumnName: React.Dispatch<React.SetStateAction<string[]>>;
+  table: Table<IFormattedLapData>;
+}) {
+  const { currentAppState } = useAppState();
+  function checkBoxFormatting(text: string) {
+    return text
+      .replace(/^.*?_/g, "")
+      .replace(/_/g, " ")
+      .replace(/([a-z])([A-Z])/g, "$1 $2")
+      .replace(/^\w/, (c) => c.toUpperCase());
+  }
+
+  const handleChange = useCallback(
+    (event: SelectChangeEvent<typeof columnName>) => {
+      const {
+        target: { value },
+      } = event;
+
+      setColumnName(typeof value === "string" ? value.split(",") : value);
+
+      table.getAllLeafColumns().forEach((col) => {
+        const columnInstance = table.getColumn(col.id);
+        if (columnInstance) {
+          columnInstance.toggleVisibility(value.includes(col.id));
+        }
+      });
+    },
+    [table, setColumnName],
+  );
+  return (
+    <FormControl
+      sx={{
+        display: "flex",
+        maxWidth: "100%",
+        minWidth: "16rem",
+      }}
+    >
+      <InputLabel
+        sx={{
+          "&.Mui-focused": {
+            color: "#963A56",
+          },
+          "&.MuiInputLabel-shrink": {
+            color: "#963A56",
+          },
+          color: "#963A56",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+        }}
+      >
+        Column
+      </InputLabel>
+
+      <Select
+        className="max-w-56 overflow-auto"
+        input={<OutlinedInput label="Column" />}
+        multiple
+        onChange={handleChange}
+        renderValue={(selected) => {
+          return selected.map((value) => checkBoxFormatting(value)).join(", ");
+        }}
+        sx={{
+          "& .MuiMenuItem-root": {
+            "&:hover": {
+              backgroundColor: "#963A56",
+            },
+            backgroundColor: "#B94A6C",
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#B94A6C",
+          },
+          "& .MuiSelect-icon": {
+            color: "#963A56",
+          },
+          "& .MuiSelect-select": {
+            color: currentAppState.darkMode ? "#D2D2D2" : "black",
+          },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#963A56",
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: currentAppState.darkMode ? "white" : "",
+          },
+        }}
+        value={columnName}
+      >
+        {table.getAllLeafColumns().map(
+          (column, index) =>
+            index !== 0 && (
+              <MenuItem key={checkBoxFormatting(column.id)} value={column.id}>
+                {checkBoxFormatting(column.id)}
+              </MenuItem>
+            ),
+        )}
+      </Select>
+    </FormControl>
+  );
+}
---
+++ packages/client/src/components/molecules/RaceTabMolecules/DriverFilter.tsx
@@ -0,0 +1,88 @@
+import { useAppState } from "@/contexts/AppStateContext";
+import { ContentCopy, ContentCopyTwoTone } from "@mui/icons-material";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { type SelectChangeEvent } from "@mui/material/Select";
+
+/*
+ * This component is used to filter the drivers of the lap data table
+ * Each specific driver is shown in the dropdown and will have their specific laps based on their Rfid
+ */
+export default function DriverFilter({
+  Rfid,
+  copy,
+  driverData,
+  handleCopy,
+  handleDropdown,
+}: {
+  driverData: { Rfid: string; driver: string }[];
+  handleDropdown: (event: SelectChangeEvent<string>) => void;
+  handleCopy: () => void;
+  Rfid: string | number;
+  copy: number;
+}) {
+  const { currentAppState } = useAppState();
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <Box className="min-w-[120px]" component="div">
+        <FormControl fullWidth>
+          <InputLabel
+            sx={{
+              "&.Mui-focused": {
+                color: "#963A56",
+              },
+              "&.MuiInputLabel-shrink": {
+                color: "#963A56",
+              },
+              color: "#963A56",
+            }}
+          >
+            Driver
+          </InputLabel>
+          <Select
+            label="Driver"
+            onChange={handleDropdown}
+            sx={{
+              "& .MuiMenuItem-root": {
+                "&:hover": {
+                  backgroundColor: "#963A56",
+                },
+                backgroundColor: "#B94A6C",
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#B94A6C",
+              },
+              "& .MuiSelect-icon": {
+                color: "#963A56",
+              },
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#963A56",
+              },
+              "&:hover .MuiOutlinedInput-notchedOutline": {
+                borderColor: currentAppState.darkMode ? "white" : "",
+              },
+            }}
+            value={Rfid?.toString()}
+          >
+            <MenuItem value={"Show all data"}>Show all data</MenuItem>
+            {driverData.map((driver) => (
+              <MenuItem key={driver.Rfid} value={driver.Rfid}>
+                {driver.driver ? `${driver.driver}` : `NO NAME`}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      {Number.isNaN(Rfid) || Rfid === "Show all data" ? "" : Rfid}
+      {Rfid && Rfid !== "Show all data" ? (
+        <button className="items-center" onClick={handleCopy}>
+          {copy === 0 ? <ContentCopy /> : <ContentCopyTwoTone />}
+        </button>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
