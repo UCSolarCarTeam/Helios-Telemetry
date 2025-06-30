409bc31950cf862a0fca6619f29469773931fb48
burtonjong <burtonjong05@gmail.com>
2025-06-29 19:09:52 -0600

    fix: change gps lap table arn ref

--- packages/server/src/datasources/DynamoDB/DynamoDB.ts
+++ packages/server/src/datasources/DynamoDB/DynamoDB.ts
@@ -21,25 +21,25 @@ import {
 } from "@aws-sdk/lib-dynamodb";
 import type { ILapData, ITelemetryData } from "@shared/helios-types";
 
-// if (!process.env.LAP_TABLE_NAME) {
-//   throw new Error("Lap table name not defined");
-// }
+if (!process.env.LAP_TABLE_NAME) {
+  throw new Error("Lap table name not defined");
+}
 
-// if (!process.env.PACKET_TABLE_NAME) {
-//   throw new Error("Packet table name not defined");
-// }
+if (!process.env.PACKET_TABLE_NAME) {
+  throw new Error("Packet table name not defined");
+}
 
-// if (!process.env.DRIVER_TABLE_NAME) {
-//   throw new Error("Driver table name not defined");
-// }
+if (!process.env.DRIVER_TABLE_NAME) {
+  throw new Error("Driver table name not defined");
+}
 
-// if (!process.env.GPS_TABLE_NAME) {
-//   throw new Error("GPS lap table name not defined");
-// }
+if (!process.env.GPS_CALCULATED_LAP_DATA_TABLE) {
+  throw new Error("GPS lap table name not defined");
+}
 
 const packetTableName = process.env.PACKET_TABLE_NAME;
 const lapTableName = process.env.LAP_TABLE_NAME;
-const gpsTableName = process.env.GPS_TABLE_NAME;
+const gpsTableName = process.env.GPS_CALCULATED_LAP_DATA_TABLE;
 const driverTableName = process.env.DRIVER_TABLE_NAME;
 
 const logger = createLightweightApplicationLogger("DynamoDB.ts");
