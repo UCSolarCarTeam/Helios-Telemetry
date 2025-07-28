2bf4487df434c5246ea21e05e5c26e7b70f0f852
DavidBarrios <barrios.david@versa-networks.com>
2025-07-27 18:50:49 -0600

    temp save

--- packages/infra/main.tf
+++ packages/infra/main.tf
@@ -9,6 +9,25 @@ module "s3_bucket"{
 }
 # module code pipeline
 
+resource "aws_ecr_repository" "default"{
+    name = "Helios Telemetry ECR"
+    description = "This is where we store our images that are used to buld our images."
+}
+# import {
+#   to = aws_ecr_repository.service
+#   id = "test-service"
+# }
+# TODO: We can migrate our existing one over but tbh it doesn't even matter:
+
+resource "aws_codebuild_project" "TelemetryCodeBuild"{
+    name = "Helios-Telemetry-CodeBuild"
+    description = "This codebuild will contain all the information about our codebuild project."
+
+    build_timeout = 5
+}
+resource "aws_codebuild_webhook" "main"{
+    project_name = aws_codebuild_project.
+}
 
 # module infrastcutre ops
 
