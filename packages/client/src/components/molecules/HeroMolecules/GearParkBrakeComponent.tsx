f64d9f6b1fdad66e36212adef55437b7ddb9423b
Dastine <christine52jesus@gmail.com>
2025-09-27 14:14:15 -0600

    [bug] Fix dark mode iccon issue.

--- packages/client/src/components/molecules/HeroMolecules/GearParkBrakeComponent.tsx
+++ packages/client/src/components/molecules/HeroMolecules/GearParkBrakeComponent.tsx
@@ -1,12 +1,17 @@
+import { useTheme } from "next-themes";
+
 import ParkingBrakeIcon from "@/components/atoms/ParkingBreakIcon";
 import { usePacket } from "@/contexts/PacketContext";
 
 function GearParkBrakeComponent() {
   const { currentPacket } = usePacket();
+  const { resolvedTheme } = useTheme();
   const selectedCSS = "font-bold text-helios";
   const reverse = currentPacket.B3.ReverseDigital;
   const forward = !currentPacket.B3.ReverseDigital;
   const brake = currentPacket.B3.BrakeSwitchDigital;
+  const colorTheme = resolvedTheme === "dark" ? "white" : "black";
+
   return (
     <>
       <div className="flex flex-col items-center">
@@ -19,7 +24,7 @@ function GearParkBrakeComponent() {
         <div className="grid grid-rows-1 gap-4">
           <div className="row-start-4 row-end-4">
             <ParkingBrakeIcon
-              color={brake ? "#B94A6C" : "#000000"}
+              color={brake ? "#B94A6C" : colorTheme}
               height="30px"
               width="30px"
             />
