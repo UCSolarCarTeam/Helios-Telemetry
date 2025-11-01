24de225d54cf97b8744acd9380d151407e4db6c9
burtonjong <burtonjong05@gmail.com>
2025-11-01 11:32:33 -0600

    fix: port fix

--- packages/db/src/data-source.ts
+++ packages/db/src/data-source.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   // database name
-  database: process.env.DB_NAME || "postgres",
+  database: "postgres",
   // entity schemas, whenever you make a table you have to add it here
   entities: [TestTable],
   // database host (if it's localhost or not)
@@ -20,10 +20,10 @@ export const AppDataSource = new DataSource({
   logging: process.env.NODE_ENV === "development",
   // the migrations directory (not sure if this works yet)
   migrations: [__dirname + "/migrations/*.{js,ts}"],
-  password: process.env.DB_PASSWORD || "postgres",
-  port: parseInt(process.env.DB_PORT || "5432"),
+  password: "postgres",
+  port: parseInt(process.env.DB_PORT || "5433"),
   //  Idk if this should be enabled or not
   //  synchronize: process.env.NODE_ENV === "development", // Only in development
   type: "postgres",
-  username: process.env.DB_USER || "postgres",
+  username: "postgres",
 });
